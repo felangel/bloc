e2205ec801a2164f5819faa86b33be806d134d1c
Felix Angelov <felangelov@gmail.com>
2018-11-30 12:53:27 -0600

    use StatelessWidget instead of InheritedWidget

--- packages/flutter_bloc/lib/src/bloc_provider.dart
+++ packages/flutter_bloc/lib/src/bloc_provider.dart
@@ -5,29 +5,31 @@ import 'package:bloc/bloc.dart';
 /// A Flutter widget which provides a bloc to its children via `BlocProvider.of(context)`.
 /// It is used as a DI widget so that a single instance of a bloc can be provided
 /// to multiple widgets within a subtree.
-class BlocProvider<T extends Bloc<dynamic, dynamic>> extends InheritedWidget {
+class BlocProvider<T extends Bloc<dynamic, dynamic>> extends StatelessWidget {
   /// The Bloc which is to be made available throughout the subtree
   final T bloc;
+
+  /// The Widget and its descendants which will have access to the Bloc.
+  final Widget child;
 
   BlocProvider({
     Key key,
     @required this.bloc,
-    @required Widget child,
+    @required this.child,
   })  : assert(bloc != null),
-        super(
-          key: key,
-          child: child,
-        );
+        assert(child != null),
+        super(key: key);
 
   @override
-  bool updateShouldNotify(BlocProvider oldWidget) => oldWidget.bloc != bloc;
+  Widget build(BuildContext context) => child;
 
   /// Method that allows widgets to access the bloc as long as their `BuildContext`
   /// contains a `BlocProvider` instance.
-  static B of<B extends Bloc<dynamic, dynamic>>(BuildContext context) {
-    Type typeOf<B>() => B;
-    BlocProvider<B> provider =
-        context.ancestorWidgetOfExactType(typeOf<BlocProvider<B>>());
+  static T of<T extends Bloc<dynamic, dynamic>>(BuildContext context) {
+    BlocProvider<T> provider =
+        context.ancestorWidgetOfExactType(_typeOf<BlocProvider<T>>());
     return provider.bloc;
   }
+
+  static Type _typeOf<T>() => T;
 }
