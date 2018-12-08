827ebe5381e598db7297046667fa293f99b46b33
Felix Angelov <felangelov@gmail.com>
2018-12-08 10:20:43 -0600

    fix typos in flutter_bloc docs and add FlutterError when BlocProvider not found (#26)

--- packages/flutter_bloc/lib/src/bloc_builder.dart
+++ packages/flutter_bloc/lib/src/bloc_builder.dart
@@ -6,12 +6,12 @@ import 'package:bloc/bloc.dart';
 
 /// A function that will be run which takes the [BuildContext] and state
 /// and is responsible for returning a [Widget] which is to be rendered.
-/// This is analagous to the `builder` function in [StreamBuilder].
+/// This is analogous to the `builder` function in [StreamBuilder].
 typedef Widget BlocWidgetBuilder<S>(BuildContext context, S state);
 
 /// A Flutter widget which requires a [Bloc] and a [BlocWidgetBuilder] `builder` function.
 /// [BlocBuilder] handles building the widget in response to new states.
-/// BlocBuilder analagous to [StreamBuilder] but has simplified API
+/// BlocBuilder analogous to [StreamBuilder] but has simplified API
 /// to reduce the amount of boilerplate code needed
 /// as well as bloc-specific performance improvements.
 class BlocBuilder<E, S> extends BlocBuilderBase<E, S> {
