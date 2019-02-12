47621b063eca2e3d3c64512ec0fabdfb298e0790
Felix Angelov <felangelov@gmail.com>
2019-02-11 20:26:06 -0600

    handle exceptions in mapEventToState (#97)

---
+++ packages/bloc/test/helpers/counter/counter_exception_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+
+import '../counter/counter_bloc.dart';
+
+class CounterExceptionBloc extends Bloc<CounterEvent, int> {
+  @override
+  int get initialState => 0;
+
+  @override
+  Stream<int> mapEventToState(int currentState, CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.decrement:
+        yield currentState - 1;
+        break;
+      case CounterEvent.increment:
+        throw Exception('fatal exception');
+        break;
+    }
+  }
+}
