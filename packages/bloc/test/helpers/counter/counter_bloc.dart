38e41aede3a29b720131b1c6b68b37960d218c52
Felix Angelov <felangelov@gmail.com>
2019-03-07 22:54:37 -0600

    import dart:async for legacy support in examples and tests

--- packages/bloc/test/helpers/counter/counter_bloc.dart
+++ packages/bloc/test/helpers/counter/counter_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 
 typedef OnTransitionCallback = Function(Transition<CounterEvent, int>);
--- packages/bloc/test/helpers/simple/simple_bloc.dart
+++ packages/bloc/test/helpers/simple/simple_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:rxdart/rxdart.dart';
 import 'package:bloc/bloc.dart';
 
