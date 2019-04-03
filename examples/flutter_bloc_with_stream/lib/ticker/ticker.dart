cb321e013e721079acee15187d0647ff8d2c3757
Jorge Coca <jorge.coca@bmwna.com>
2019-04-03 16:20:42 -0500

    do not use fat arrow

--- examples/flutter_bloc_with_stream/lib/ticker/ticker.dart
+++ examples/flutter_bloc_with_stream/lib/ticker/ticker.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 class Ticker {
-  Stream<int> tick() =>
-      Stream.periodic(Duration(seconds: 1), (x) => x).take(10);
+  Stream<int> tick() {
+    return Stream.periodic(Duration(seconds: 1), (x) => x).take(10);
+  }
 }
