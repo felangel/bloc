f374ba60b7ae19adbfe271ff04bb3f5da853ead1
Felix Angelov <felangelov@gmail.com>
2019-05-31 20:01:31 -0500

    add hydrated_bloc_delegate test

--- test/hydrated_bloc_delegate_test.dart
+++ test/hydrated_bloc_delegate_test.dart
@@ -1,7 +1,31 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:bloc/bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+
+class MockBloc extends Mock implements HydratedBloc<dynamic, dynamic> {}
+
+class MockStorage extends Mock implements HydratedBlocStorage {}
 
 void main() {
-  test('sample', () {
-    expect(true, true);
+  MockStorage storage;
+  HydratedBlocDelegate delegate;
+  MockBloc bloc;
+
+  setUp(() {
+    storage = MockStorage();
+    delegate = HydratedBlocDelegate(storage);
+    bloc = MockBloc();
+  });
+
+  test('call storage.write when onTransition is called', () {
+    final transition = Transition(
+      currentState: 'currentState',
+      event: 'event',
+      nextState: 'nextState',
+    );
+    when(bloc.toJson('nextState')).thenReturn('nextStateJson');
+    delegate.onTransition(bloc, transition);
+    verify(storage.write('MockBloc', 'nextStateJson')).called(1);
   });
 }
