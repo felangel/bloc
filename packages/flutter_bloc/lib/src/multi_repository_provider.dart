8d7f298d95bfca1c1b8c5a6d4406b9a25c1d843c
Micheal <micheal.reed@outlook.com>
2019-07-10 14:10:18 -0500

    changed flutter to flutter_web

--- packages/flutter_bloc/lib/src/multi_repository_provider.dart
+++ packages/flutter_bloc/lib/src/multi_repository_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter_web/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
--- packages/flutter_bloc/lib/src/repository_provider.dart
+++ packages/flutter_bloc/lib/src/repository_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter_web/widgets.dart';
 import 'package:meta/meta.dart';
 import 'package:provider/provider.dart';
 
