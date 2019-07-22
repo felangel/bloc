11fb3b21747a967117f1f3a2e4a775b2540b0ce7
Felix Angelov <felangelov@gmail.com>
2019-07-21 20:48:43 -0500

    initial commit

---
+++ examples/flutter_firestore_todos/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(FirestoreTodos());
+
+class FirestoreTodos extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
---
+++ examples/flutter_firestore_todos/todos_repository/lib/todos_repository.dart
@@ -0,0 +1,3 @@
+library todos_repository;
+
+export 'src/todos_repository.dart';
