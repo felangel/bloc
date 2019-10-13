ebcd2392412ef67092b55b83f4a4f8765d224458
Felix Angelov <felangelov@gmail.com>
2019-10-13 15:18:26 -0500

    minor updates to example

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -1,12 +1,9 @@
 //
 //  Generated file. Do not edit.
 //
-
-import FlutterMacOS
 import Foundation
+import FlutterMacOS
 
-import path_provider_fde
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
