5e50acb3161b3ab1b11d99f2c4b8335f8d89aade
Felix Angelov <felangelov@gmail.com>
2019-10-27 22:08:57 -0500

    update effective_dart to v1.1.0

--- packages/bloc/lib/src/transition.dart
+++ packages/bloc/lib/src/transition.dart
@@ -1,18 +1,18 @@
 import 'package:meta/meta.dart';
 
 /// {@template transition}
-/// Occurs when an [Event] is `added` after `mapEventToState` has been called
-/// but before the `Bloc`'s [State] has been updated.
-/// A [Transition] consists of the `currentState`, the `event` which was `added`, and the `nextState`.
+/// Occurs when an [event] is `added` after `mapEventToState` has been called
+/// but before the [bloc]'s `state` has been updated.
+/// A [Transition] consists of the [currentState], the [event] which was `added`, and the [nextState].
 /// {@endtemplate}
 class Transition<Event, State> {
-  /// The current [State] of the bloc at the time of the [Transition].
+  /// The current [State] of the [bloc] at the time of the [Transition].
   final State currentState;
 
   /// The [Event] which triggered the current [Transition].
   final Event event;
 
-  /// The next [State] of the bloc at the time of the [Transition].
+  /// The next [State] of the [bloc] at the time of the [Transition].
   final State nextState;
 
   /// {@macro transition}
