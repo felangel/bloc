a200765d070f42f913ecb2df647ca18d5c3a516e
Felix Angelov <felangelov@gmail.com>
2019-11-30 20:46:37 -0600

    examples/flutter_firebase_login

--- examples/flutter_firebase_login/lib/login/bloc/login_bloc.dart
+++ examples/flutter_firebase_login/lib/login/bloc/login_bloc.dart
@@ -22,15 +22,16 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     Stream<LoginEvent> events,
     Stream<LoginState> Function(LoginEvent event) next,
   ) {
-    final observableStream = events as Observable<LoginEvent>;
-    final nonDebounceStream = observableStream.where((event) {
+    final nonDebounceStream = events.where((event) {
       return (event is! EmailChanged && event is! PasswordChanged);
     });
-    final debounceStream = observableStream.where((event) {
+    final debounceStream = events.where((event) {
       return (event is EmailChanged || event is PasswordChanged);
     }).debounceTime(Duration(milliseconds: 300));
-    return super
-        .transformEvents(nonDebounceStream.mergeWith([debounceStream]), next);
+    return super.transformEvents(
+      nonDebounceStream.mergeWith([debounceStream]),
+      next,
+    );
   }
 
   @override
