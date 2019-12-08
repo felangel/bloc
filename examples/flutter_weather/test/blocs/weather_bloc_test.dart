72fc0184149e94fc5410f0e60e2300febb87cd34
Felix Angelov <felangelov@gmail.com>
2019-12-07 20:31:07 -0500

    examples/flutter_weather

--- examples/flutter_weather/test/blocs/weather_bloc_test.dart
+++ examples/flutter_weather/test/blocs/weather_bloc_test.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_weather/repositories/weather_repository.dart';
 import 'package:flutter_weather/models/weather.dart';
