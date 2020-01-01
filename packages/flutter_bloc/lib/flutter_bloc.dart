6a13a47b3bd27970464a4166272f75fba0a39563
Felix Angelov <felangelov@gmail.com>
2020-01-01 14:49:40 -0600

    export BlocConsumer

--- packages/flutter_bloc/lib/flutter_bloc.dart
+++ packages/flutter_bloc/lib/flutter_bloc.dart
@@ -3,6 +3,7 @@ library flutter_bloc;
 export 'package:bloc/bloc.dart';
 
 export './src/bloc_builder.dart';
+export './src/bloc_consumer.dart';
 export './src/bloc_listener.dart';
 export './src/bloc_provider.dart';
 export './src/multi_bloc_listener.dart';
--- packages/flutter_bloc/test/bloc_consumer_test.dart
+++ packages/flutter_bloc/test/bloc_consumer_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_bloc/src/bloc_consumer.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 enum CounterEvent { increment, decrement }
