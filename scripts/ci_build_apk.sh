1de79e78542e250ff382251836a7dedddbaa8d30
Anthony Verez <netantho@gmail.com>
2020-02-01 22:53:09 +0100

    Build flutter_firebase_login and flutter_firestore_todos with CI

--- scripts/ci_build_apk.sh
+++ scripts/ci_build_apk.sh
@@ -6,6 +6,9 @@ source scripts/retry.sh
 
 cd $1
 
+if [[ -f "android/app/example_google-services.json" ]]; then
+  cp android/app/example_google-services.json android/app/google-services.json
+fi
 flutter packages get
 flutter build apk
 
