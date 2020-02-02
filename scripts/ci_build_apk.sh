dc9b5b02c8e348ad9bcf88bd4d238bac8807a27c
Anthony Verez <netantho@gmail.com>
2020-02-02 09:33:30 +0100

    Pass --debug to `flutter build apk`

--- scripts/ci_build_apk.sh
+++ scripts/ci_build_apk.sh
@@ -10,6 +10,6 @@ if [[ -f "android/app/example_google-services.json" ]]; then
   cp android/app/example_google-services.json android/app/google-services.json
 fi
 flutter packages get
-flutter build apk
+flutter build apk --debug
 
 cd -
