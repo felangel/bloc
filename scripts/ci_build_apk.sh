46a44d0ee02ec1917a9f55b64971999d95ed0236
Anthony Verez <netantho@gmail.com>
2020-02-02 22:57:23 +0100

    Analyze performance tests and migrate them to AndroidX

--- scripts/ci_build_apk.sh
+++ scripts/ci_build_apk.sh
@@ -7,9 +7,11 @@ source scripts/retry.sh
 cd $1
 
 if [[ -f "android/app/example_google-services.json" ]]; then
-  cp android/app/example_google-services.json android/app/google-services.json
+    cp android/app/example_google-services.json android/app/google-services.json
 fi
 flutter packages get
-flutter build apk
+if [[ -f "lib/main.dart" ]]; then
+    flutter build apk
+fi
 
 cd -
