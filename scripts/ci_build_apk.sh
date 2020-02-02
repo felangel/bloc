a51d5ccd73240cb402f29e16b01123663a843377
Anthony Verez <netantho@gmail.com>
2020-02-02 10:15:06 +0100

    Revert "Pass --debug to `flutter build apk`"
    This reverts commit dc9b5b02c8e348ad9bcf88bd4d238bac8807a27c.

--- scripts/ci_build_apk.sh
+++ scripts/ci_build_apk.sh
@@ -10,6 +10,6 @@ if [[ -f "android/app/example_google-services.json" ]]; then
   cp android/app/example_google-services.json android/app/google-services.json
 fi
 flutter packages get
-flutter build apk --debug
+flutter build apk
 
 cd -
