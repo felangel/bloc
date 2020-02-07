9efdbd3c57c7067a76f182f9e18ef6b7dd85be17
Honza Bittner <git@tenhobi.dev>
2020-02-07 20:47:20 +0100

    Remove performance tests from CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,23 +10,19 @@ folder=${PWD##*/}
 
 if grep -q 'sdk: flutter' "./pubspec.yaml"; then
     flutter packages get
-    if [[ -d "$folder/lib" ]]; then
-        flutter format --set-exit-if-changed lib
-        flutter analyze --no-current-package lib
-    fi
+    flutter format --set-exit-if-changed lib
+    flutter analyze --no-current-package lib
+
     if [[ -d "$folder/test" ]]; then
         flutter format --set-exit-if-changed test
         flutter analyze --no-current-package test
         flutter test --no-pub --coverage
     fi
-    if [[ -d "$folder/test_driver" ]]; then
-        flutter format --set-exit-if-changed test_driver
-        flutter analyze --no-current-package test_driver
-    fi
 elif grep -q 'angular:' "./pubspec.yaml"; then
     pub get
     dartfmt -w .
     dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
+
     if [[ -d "$folder/test" ]]; then
         pub run build_runner test --fail-on-severe
     fi
@@ -34,6 +30,7 @@ else
     pub get
     dartfmt -w .
     dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
+
     if [[ -d "$folder/test" ]]; then
         retry pub run test_coverage
     fi
