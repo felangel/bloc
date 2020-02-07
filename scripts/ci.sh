f4d6013bf97a8d200411dfca2598217fc832d98c
Honza Bittner <git@tenhobi.dev>
2020-02-07 20:05:33 +0100

    Add iOS for Flutter examples build; enhance CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+# inputs $1: project location
 
 set -ex
 
 source scripts/retry.sh
 
 cd $1
-package=${PWD##*/}
+folder=${PWD##*/}
 
 if grep -q 'sdk: flutter' "./pubspec.yaml"; then
     flutter packages get
-    if [[ -f "$package/lib" ]]; then
+    if [[ -f "$folder/lib" ]]; then
         flutter format --set-exit-if-changed lib
         flutter analyze --no-current-package lib
     fi
-    if [[ -f "$package/test" ]]; then
+    if [[ -f "$folder/test" ]]; then
         flutter format --set-exit-if-changed test
         flutter analyze --no-current-package test
         flutter test --no-pub --coverage
-        cp ./coverage/lcov.info ../../$package.lcov
+        cp ./coverage/lcov.info ../../$folder.lcov
     fi
-    if [[ -f "$package/test_driver" ]]; then
+    if [[ -f "$folder/test_driver" ]]; then
         flutter format --set-exit-if-changed test_driver
         flutter analyze --no-current-package test_driver
     fi
@@ -27,16 +28,16 @@ elif grep -q 'angular:' "./pubspec.yaml"; then
     pub get
     dartfmt -w .
     dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
-    if [[ -f "$package/test" ]]; then
+    if [[ -f "$folder/test" ]]; then
         pub run build_runner test --fail-on-severe
     fi
 else
     pub get
     dartfmt -w .
     dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
-    if [[ -f "$package/test" ]]; then
+    if [[ -f "$folder/test" ]]; then
         retry pub run test_coverage
-        cp ./coverage/lcov.info ../../$package.lcov
+        cp ./coverage/lcov.info ../../$folder.lcov
     fi
 fi
 
