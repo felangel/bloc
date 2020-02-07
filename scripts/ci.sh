983da1afea9957f05e184fd4d8d46f9b55fbf092
Honza Bittner <git@tenhobi.dev>
2020-02-07 20:29:15 +0100

    Make build files executable; update -f to -d

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -10,17 +10,17 @@ folder=${PWD##*/}
 
 if grep -q 'sdk: flutter' "./pubspec.yaml"; then
     flutter packages get
-    if [[ -f "$folder/lib" ]]; then
+    if [[ -d "$folder/lib" ]]; then
         flutter format --set-exit-if-changed lib
         flutter analyze --no-current-package lib
     fi
-    if [[ -f "$folder/test" ]]; then
+    if [[ -d "$folder/test" ]]; then
         flutter format --set-exit-if-changed test
         flutter analyze --no-current-package test
         flutter test --no-pub --coverage
         cp ./coverage/lcov.info ../../$folder.lcov
     fi
-    if [[ -f "$folder/test_driver" ]]; then
+    if [[ -d "$folder/test_driver" ]]; then
         flutter format --set-exit-if-changed test_driver
         flutter analyze --no-current-package test_driver
     fi
@@ -28,14 +28,14 @@ elif grep -q 'angular:' "./pubspec.yaml"; then
     pub get
     dartfmt -w .
     dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
-    if [[ -f "$folder/test" ]]; then
+    if [[ -d "$folder/test" ]]; then
         pub run build_runner test --fail-on-severe
     fi
 else
     pub get
     dartfmt -w .
     dartanalyzer --fatal-infos --fatal-warnings . || EXIT_CODE=$?
-    if [[ -f "$folder/test" ]]; then
+    if [[ -d "$folder/test" ]]; then
         retry pub run test_coverage
         cp ./coverage/lcov.info ../../$folder.lcov
     fi
