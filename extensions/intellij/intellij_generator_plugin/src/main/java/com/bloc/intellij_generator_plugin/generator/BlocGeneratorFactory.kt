5375f15457f26cbb3edbe08f3a62dfa4fef0cfe2
Felix Angelov <felangelov@gmail.com>
2020-04-15 20:59:54 -0500

    Upgrade IntelliJ Plugin to v1.7.0

--- extensions/intellij/intellij_generator_plugin/src/main/java/com/bloc/intellij_generator_plugin/generator/BlocGeneratorFactory.kt
+++ extensions/intellij/intellij_generator_plugin/src/main/java/com/bloc/intellij_generator_plugin/generator/BlocGeneratorFactory.kt
@@ -1,17 +1,15 @@
 package com.bloc.intellij_generator_plugin.generator
 
 import com.bloc.intellij_generator_plugin.generator.components.BlocEventGenerator
-import com.bloc.intellij_generator_plugin.generator.components.BlocExportsGenerator
 import com.bloc.intellij_generator_plugin.generator.components.BlocGenerator
 import com.bloc.intellij_generator_plugin.generator.components.BlocStateGenerator
 
 object BlocGeneratorFactory {
 
     fun getBlocGenerators(blocName: String, blocShouldUseEquatable: Boolean): List<Generator> {
-        val exports = BlocExportsGenerator(blocName, blocShouldUseEquatable)
         val bloc = BlocGenerator(blocName, blocShouldUseEquatable)
         val event = BlocEventGenerator(blocName, blocShouldUseEquatable)
         val state = BlocStateGenerator(blocName, blocShouldUseEquatable)
-        return listOf(exports, bloc, event, state)
+        return listOf(bloc, event, state)
     }
 }
