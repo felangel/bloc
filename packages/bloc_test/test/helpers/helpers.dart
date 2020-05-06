0fbbc9c7f661ad600cb75fa09be9ee90e2bc0301
Felix Angelov <felangelov@gmail.com>
2020-05-06 14:46:33 -0500

    bloc_test v5.1.0 (#1129)

--- packages/bloc_test/test/helpers/helpers.dart
+++ packages/bloc_test/test/helpers/helpers.dart
@@ -2,6 +2,7 @@ export 'async_counter_bloc.dart';
 export 'complex_bloc.dart';
 export 'counter_bloc.dart';
 export 'debounce_counter_bloc.dart';
+export 'exception_counter_bloc.dart';
 export 'multi_counter_bloc.dart';
 export 'side_effect_counter_bloc.dart';
 export 'sum_bloc.dart';
