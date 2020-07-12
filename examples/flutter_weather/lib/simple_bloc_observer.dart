69b8174d30fe75965e34e00392727eafbf430e04
Felix Angelov <felangelov@gmail.com>
2020-07-11 19:07:10 -0500

    refactor(flutter_bloc): update example apps

--- examples/flutter_weather/lib/simple_bloc_observer.dart
+++ examples/flutter_weather/lib/simple_bloc_observer.dart
@@ -14,8 +14,8 @@ class SimpleBlocObserver extends BlocObserver {
   }
 
   @override
-  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
-    super.onError(bloc, error, stackTrace);
+  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+    super.onError(cubit, error, stackTrace);
     print('onError $error');
   }
 }
