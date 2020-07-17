430f3e7125e8b9e5b712f108abcc588820355361
Felix Angelov <felangelov@gmail.com>
2020-07-17 18:25:47 -0500

    fix(hydrated_bloc): handle empty case for list traversal (#1482)

--- packages/hydrated_bloc/test/e2e_test.dart
+++ packages/hydrated_bloc/test/e2e_test.dart
@@ -231,6 +231,33 @@ void main() {
           <ListCustomObject>[item],
         );
       });
+
+      test('persists and restores obj->list<custom> empty list correctly',
+          () async {
+        const fromJson = ListCustomObject.fromJson;
+        final cubit = ListCubitList<ListCustomObject, CustomObject>(fromJson);
+        expect(cubit.state, isEmpty);
+        cubit.reset();
+        await sleep();
+        expect(
+          ListCubitList<ListCustomObject, CustomObject>(fromJson).state,
+          isEmpty,
+        );
+      });
+
+      test('persists and restores obj-*>list<custom> empty list correctly',
+          () async {
+        const fromJson = ListCustomObject.fromJson;
+        final cubit =
+            ListCubitList<ListCustomObject, CustomObject>(fromJson, true);
+        expect(cubit.state, isEmpty);
+        cubit.reset();
+        await sleep();
+        expect(
+          ListCubitList<ListCustomObject, CustomObject>(fromJson).state,
+          isEmpty,
+        );
+      });
     });
 
     group('ManualCubit', () {
