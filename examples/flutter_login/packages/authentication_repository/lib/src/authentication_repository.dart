ba4831aa0f732d44b104dba8eed9079f823d2902
Felix Angelov <felangelov@gmail.com>
2020-07-24 14:37:54 -0500

    fix analysis error

--- examples/flutter_login/packages/authentication_repository/lib/src/authentication_repository.dart
+++ examples/flutter_login/packages/authentication_repository/lib/src/authentication_repository.dart
@@ -8,7 +8,7 @@ class AuthenticationRepository {
   final _controller = StreamController<AuthenticationStatus>();
 
   Stream<AuthenticationStatus> get status async* {
-    await Future.delayed(const Duration(seconds: 1));
+    await Future<void>.delayed(const Duration(seconds: 1));
     yield AuthenticationStatus.unauthenticated;
     yield* _controller.stream;
   }
