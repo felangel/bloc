b59f60e39a2171d14a9245a05e298bb78db28495
QAQ <z513873698@gmail.com>
2020-07-25 12:01:12 +0100

    added create new freezed bloc command

--- extensions/vscode/src/commands/index.ts
+++ extensions/vscode/src/commands/index.ts
@@ -1,2 +1,3 @@
 export * from "./new-bloc.command";
 export * from "./new-cubit.command";
+export * from "./new-freezed-bloc.command";
--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -2,10 +2,11 @@ import * as _ from "lodash";
 
 import { commands, ExtensionContext } from "vscode";
 import { analyzeDependencies } from "./utils";
-import { newBloc, newCubit } from "./commands";
+import { newBloc, newCubit, newFreezedBloc } from "./commands";
 
 export function activate(_context: ExtensionContext) {
   analyzeDependencies();
   commands.registerCommand("extension.new-bloc", newBloc);
   commands.registerCommand("extension.new-cubit", newCubit);
+  commands.registerCommand("extension.new-frezzed-bloc", newFreezedBloc);
 }
