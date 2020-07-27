aafeed1528a0b74b9f15334bffd6c9088fb14f30
Felix Angelov <felangelov@gmail.com>
2020-07-26 22:19:15 -0500

    docs(flutter_counter): create examples/flutter_counter

---
+++ examples/flutter_counter/lib/counter/cubit/counter_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+}
---
+++ examples/flutter_counter/test/app_test.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:flutter_counter/app.dart';
+import 'package:flutter_counter/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CounterApp', () {
+    testWidgets('is a MaterialApp', (tester) async {
+      expect(CounterApp(), isA<MaterialApp>());
+    });
+
+    testWidgets('home is CounterPage', (tester) async {
+      expect(CounterApp().home, isA<CounterPage>());
+    });
+
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(CounterApp());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
