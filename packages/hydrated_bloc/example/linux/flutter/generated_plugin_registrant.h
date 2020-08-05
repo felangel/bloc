4bb425dbc167332b768c37b20c92bc4135333d25
Felix Angelov <felangelov@gmail.com>
2020-08-04 19:47:58 -0500

    fix(hydrated_bloc): add web support

--- packages/hydrated_bloc/example/linux/flutter/generated_plugin_registrant.h
+++ packages/hydrated_bloc/example/linux/flutter/generated_plugin_registrant.h
@@ -5,9 +5,9 @@
 #ifndef GENERATED_PLUGIN_REGISTRANT_
 #define GENERATED_PLUGIN_REGISTRANT_
 
-#include <flutter/plugin_registry.h>
+#include <flutter_linux/flutter_linux.h>
 
 // Registers Flutter plugins.
-void RegisterPlugins(flutter::PluginRegistry* registry);
+void fl_register_plugins(FlPluginRegistry* registry);
 
 #endif  // GENERATED_PLUGIN_REGISTRANT_
