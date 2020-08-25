9fdff133eacda90436b44cdb4376d5b1397e0c9e
QAQ <z513873698@gmail.com>
2020-08-25 19:14:00 +0100

    updated get template function to depend on the vscode configuration

--- extensions/vscode/src/utils/get-bloc-type.ts
+++ extensions/vscode/src/utils/get-bloc-type.ts
@@ -1,4 +1,9 @@
 import { hasDependency } from "./has-dependency";
+import {
+  SettingType,
+  getConfigedTemplate,
+  TemplateSetting,
+} from "./get-vscode-config";
 
 const equatable = "equatable";
 const freezed_annotation = "freezed_annotation";
@@ -9,11 +14,39 @@ export const enum BlocType {
   Freezed,
 }
 
-export async function getTemplateType() {
+export async function getTemplateType(which: SettingType) {
+  const setting = getConfigedTemplate(which);
+  switch (setting) {
+    case TemplateSetting.Default:
+      return getDefaultDependency();
+    case TemplateSetting.Reverse:
+      return getReverseDependency();
+    case TemplateSetting.Equatable:
+      return BlocType.Equatable;
+    case TemplateSetting.Freezed:
+      return BlocType.Freezed;
+    case TemplateSetting.Simple:
+      return BlocType.Simple;
+    default:
+      return getDefaultDependency();
+  }
+}
+
+async function getDefaultDependency() {
   if (await hasDependency(equatable)) {
     return BlocType.Equatable;
   } else if (await hasDependency(freezed_annotation)) {
     return BlocType.Freezed;
+  } else {
+    return BlocType.Simple;
+  }
+}
+
+async function getReverseDependency() {
+  if (await hasDependency(freezed_annotation)) {
+    return BlocType.Freezed;
+  } else if (await hasDependency(equatable)) {
+    return BlocType.Equatable;
   } else {
     return BlocType.Simple;
   }
