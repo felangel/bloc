557b514575f3f44e7bb0f8fa7d5d7f0f297e0362
Felix Angelov <felangelov@gmail.com>
2020-09-25 15:41:56 -0500

    refactor: upgrade dependencies for flutter firebase login example (#1772)

--- examples/flutter_firebase_login/lib/main.dart
+++ examples/flutter_firebase_login/lib/main.dart
@@ -3,11 +3,13 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_firebase_login/app.dart';
 import 'package:flutter_firebase_login/simple_bloc_observer.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   EquatableConfig.stringify = kDebugMode;
   Bloc.observer = SimpleBlocObserver();
   runApp(App(authenticationRepository: AuthenticationRepository()));
--- examples/flutter_firebase_login/packages/authentication_repository/test/authentication_repository_test.dart
+++ examples/flutter_firebase_login/packages/authentication_repository/test/authentication_repository_test.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:authentication_repository/authentication_repository.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mockito/mockito.dart';
@@ -7,9 +9,9 @@ import 'package:mockito/mockito.dart';
 const _mockFirebaseUserUid = 'mock-uid';
 const _mockFirebaseUserEmail = 'mock-email';
 
-class MockFirebaseAuth extends Mock implements FirebaseAuth {}
+class MockFirebaseAuth extends Mock implements firebase_auth.FirebaseAuth {}
 
-class MockFirebaseUser extends Mock implements FirebaseUser {
+class MockFirebaseUser extends Mock implements firebase_auth.User {
   @override
   String get uid => _mockFirebaseUserUid;
 
@@ -25,6 +27,38 @@ class MockGoogleSignInAuthentication extends Mock
     implements GoogleSignInAuthentication {}
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  MethodChannelFirebase.channel.setMockMethodCallHandler((call) async {
+    if (call.method == 'Firebase#initializeCore') {
+      return [
+        {
+          'name': defaultFirebaseAppName,
+          'options': {
+            'apiKey': '123',
+            'appId': '123',
+            'messagingSenderId': '123',
+            'projectId': '123',
+          },
+          'pluginConstants': const <String, String>{},
+        }
+      ];
+    }
+
+    if (call.method == 'Firebase#initializeApp') {
+      return <String, dynamic>{
+        'name': call.arguments['appName'],
+        'options': call.arguments['options'],
+        'pluginConstants': const <String, String>{},
+      };
+    }
+
+    return null;
+  });
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+
   const email = 'test@gmail.com';
   const password = 't0ps3cret42';
   const user = User(
@@ -35,7 +69,7 @@ void main() {
   );
 
   group('AuthenticationRepository', () {
-    FirebaseAuth firebaseAuth;
+    firebase_auth.FirebaseAuth firebaseAuth;
     GoogleSignIn googleSignIn;
     AuthenticationRepository authenticationRepository;
 
@@ -102,9 +136,13 @@ void main() {
     });
 
     group('loginWithGoogle', () {
+      const accessToken = 'access-token';
+      const idToken = 'id-token';
       setUp(() {
         final googleSignInAuthentication = MockGoogleSignInAuthentication();
         final googleSignInAccount = MockGoogleSignInAccount();
+        when(googleSignInAuthentication.accessToken).thenReturn(accessToken);
+        when(googleSignInAuthentication.idToken).thenReturn(idToken);
         when(
           googleSignInAccount.authentication,
         ).thenAnswer((_) async => googleSignInAuthentication);
@@ -211,7 +249,7 @@ void main() {
 
     group('user', () {
       test('emits User.empty when firebase user is null', () async {
-        when(firebaseAuth.onAuthStateChanged).thenAnswer(
+        when(firebaseAuth.authStateChanges()).thenAnswer(
           (_) => Stream.value(null),
         );
         await expectLater(
@@ -221,7 +259,7 @@ void main() {
       });
 
       test('emits User when firebase user is not null', () async {
-        when(firebaseAuth.onAuthStateChanged).thenAnswer(
+        when(firebaseAuth.authStateChanges()).thenAnswer(
           (_) => Stream.value(MockFirebaseUser()),
         );
         await expectLater(
