e8711410b547c0f3bfa6661dc9fc549dc1a2ebbe
Felix Angelov <felangelov@gmail.com>
2020-10-05 11:44:17 -0500

    minor update

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -13,7 +13,7 @@ dart \
   --pause-isolates-on-exit \
   --enable_asserts \
   --enable-vm-service=$OBS_PORT \
-  test/all_tests.dart &
+  test/main.dart &
 
 # Run the coverage collector to generate the JSON coverage report.
 echo "Collecting coverage..."
