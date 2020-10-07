b02847b1aa4744f6866f2acd1f369bc3a00fffee
Felix Angelov <felangelov@gmail.com>
2020-10-06 23:50:53 -0500

    docs(replay_bloc): add inline documentation to mixins

---
+++ packages/replay_bloc/test/main.dart
@@ -0,0 +1,7 @@
+import 'replay_bloc_test.dart' as replay_bloc_test;
+import 'replay_cubit_test.dart' as replay_cubit_test;
+
+void main() {
+  replay_cubit_test.main();
+  replay_bloc_test.main();
+}
