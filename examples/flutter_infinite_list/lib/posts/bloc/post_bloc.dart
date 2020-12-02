a54ffe9cd0e6dc7ab6b9ddd293f7847cdba056fb
Sylvanus Kateile <kateile@outlook.com>
2020-12-02 12:20:30 +0300

    fix(flutter_infinite_list): hiding bottom loader when no more posts available

--- examples/flutter_infinite_list/lib/posts/bloc/post_bloc.dart
+++ examples/flutter_infinite_list/lib/posts/bloc/post_bloc.dart
@@ -9,12 +9,16 @@ import 'package:meta/meta.dart';
 import 'package:rxdart/rxdart.dart';
 
 part 'post_event.dart';
+
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
   PostBloc({@required this.httpClient}) : super(const PostState());
 
   final http.Client httpClient;
+
+  /// Limit of posts to be fetched in each request
+  final _limit = 20;
 
   @override
   Stream<Transition<PostEvent, PostState>> transformEvents(
@@ -38,29 +42,29 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     if (state.hasReachedMax) return state;
     try {
       if (state.status == PostStatus.initial) {
-        final posts = await _fetchPosts(0, 20);
+        final posts = await _fetchPosts(0);
         return state.copyWith(
           status: PostStatus.success,
           posts: posts,
-          hasReachedMax: false,
+          hasReachedMax: _hasReachedEnd(posts.length),
         );
       }
-      final posts = await _fetchPosts(state.posts.length, 20);
+      final posts = await _fetchPosts(state.posts.length);
       return posts.isEmpty
           ? state.copyWith(hasReachedMax: true)
           : state.copyWith(
               status: PostStatus.success,
               posts: List.of(state.posts)..addAll(posts),
-              hasReachedMax: false,
+              hasReachedMax: _hasReachedEnd(posts.length),
             );
     } on Exception {
       return state.copyWith(status: PostStatus.failure);
     }
   }
 
-  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+  Future<List<Post>> _fetchPosts(int startIndex) async {
     final response = await httpClient.get(
-      'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit',
+      'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$_limit',
     );
     if (response.statusCode == 200) {
       final data = json.decode(response.body) as List;
@@ -74,4 +78,6 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     }
     throw Exception('error fetching posts');
   }
+
+  bool _hasReachedEnd(int postsCount) => postsCount < _limit ? true : false;
 }
