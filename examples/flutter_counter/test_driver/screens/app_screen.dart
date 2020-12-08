cf8d8a283f91b33487ad5d41b655f41565e2f3bf
Abhishek01039 <abhishekghaskata1999@gmail.com>
2020-12-08 22:45:02 +0530

    changes done given by felix

--- examples/flutter_counter/test_driver/screens/app_screen.dart
+++ examples/flutter_counter/test_driver/screens/app_screen.dart
@@ -2,43 +2,37 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
 class AppScreen {
-  AppScreen(FlutterDriver flutterDriver) {
-    _driver = flutterDriver;
-  }
+  const AppScreen(this._driver);
 
-  final incrementFloatingButton =
+  static final incrementFloatingButton =
       find.byValueKey('counterView_increment_floatingActionButton');
-  final decrementFloatingButton =
+  static final decrementFloatingButton =
       find.byValueKey('counterView_decrement_floatingActionButton');
-  final appBarText = find.text('Counter');
-  final counterState = find.byValueKey('counterState');
-  FlutterDriver _driver;
+  static final appBarText = find.text('Counter');
+  static final counterState = find.byValueKey('counterState');
+  final FlutterDriver _driver;
 
   /// verify the AppBar text is Counter
   Future<void> verifyTheAppBarText() async {
-    expect(await _getTextByScreen(appBarText), 'Counter');
+    expect(await _driver.getText(appBarText), 'Counter');
   }
 
   Future<void> verifyCounterTextIsZero() async {
-    expect(await _getTextByScreen(counterState), '0');
+    expect(await _driver.getText(counterState), '0');
   }
 
   Future<void> pressIncrementFloatingActionButtonTwice() async {
     // tap floating action button
     await _driver.tap(incrementFloatingButton);
-    expect(await _getTextByScreen(counterState), '1');
+    expect(await _driver.getText(counterState), '1');
 
     // tap floating action button
     await _driver.tap(incrementFloatingButton);
-    expect(await _getTextByScreen(counterState), '2');
+    expect(await _driver.getText(counterState), '2');
   }
 
   Future<void> decrementFloatingActionButton() async {
     await _driver.tap(decrementFloatingButton);
-    expect(await _getTextByScreen(counterState), '1');
-  }
-
-  Future<String> _getTextByScreen(SerializableFinder finder) async {
-    return await _driver.getText(finder);
+    expect(await _driver.getText(counterState), '1');
   }
 }
