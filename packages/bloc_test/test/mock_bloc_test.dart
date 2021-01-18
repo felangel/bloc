b8bf0fb9d55d861b355a8fd4e417aa18bed036b0
Felix Angelov <felangelov@gmail.com>
2021-01-17 23:00:15 -0600

    feat(bloc_test)!: remove skip stubs from whenListen + MockBloc auto stubs

--- packages/bloc_test/test/mock_bloc_test.dart
+++ packages/bloc_test/test/mock_bloc_test.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:bloc/bloc.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -24,6 +25,62 @@ void main() {
     test('is compatible with when', () {
       when(counterBloc).calls(#state).thenReturn(10);
       expect(counterBloc.state, 10);
+    });
+
+    test('is compatible with listen', () {
+      expect(counterBloc.listen((_) {}), isA<StreamSubscription>());
+    });
+
+    test('is compatible with isBroadcast', () {
+      expect(counterBloc.isBroadcast, isTrue);
+    });
+
+    test('is compatible with emit', () {
+      counterBloc.emit(10);
+    });
+
+    test('is compatible with add', () {
+      counterBloc.add(CounterEvent.increment);
+    });
+
+    test('is compatible with addError without StackTrace', () {
+      counterBloc.addError(Exception('oops'));
+    });
+
+    test('is compatible with addError with StackTrace', () {
+      counterBloc.addError(Exception('oops'), StackTrace.empty);
+    });
+
+    test('is compatible with onEvent', () {
+      // ignore: invalid_use_of_protected_member
+      counterBloc.onEvent(CounterEvent.increment);
+    });
+
+    test('is compatible with onError', () {
+      // ignore: invalid_use_of_protected_member
+      counterBloc.onError(Exception('oops'), StackTrace.empty);
+    });
+
+    test('is compatible with onTransition', () {
+      // ignore: invalid_use_of_protected_member
+      counterBloc.onTransition(
+        const Transition(
+          currentState: 0,
+          event: CounterEvent.increment,
+          nextState: 1,
+        ),
+      );
+    });
+
+    test('is compatible with close', () {
+      expect(counterBloc.close(), completes);
+    });
+
+    test('is compatible with mapEventToState', () {
+      expect(
+        counterBloc.mapEventToState(CounterEvent.increment),
+        isA<Stream<int>>(),
+      );
     });
 
     test('is automatically compatible with whenListen', () {
