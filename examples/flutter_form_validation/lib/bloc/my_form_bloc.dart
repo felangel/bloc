1290ba2ec8de2793395256da3f275b1791dd9c9b
Abhishek Ghaskata <abhishekghaskata1999@gmail.com>
2021-02-15 03:33:35 +0530

    refactor(flutter_form_validation): migrate to null-safety (#2148)

--- examples/flutter_form_validation/lib/bloc/my_form_bloc.dart
+++ examples/flutter_form_validation/lib/bloc/my_form_bloc.dart
@@ -3,7 +3,6 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_form_validation/models/models.dart';
 import 'package:formz/formz.dart';
-import 'package:meta/meta.dart';
 
 part 'my_form_event.dart';
 part 'my_form_state.dart';
