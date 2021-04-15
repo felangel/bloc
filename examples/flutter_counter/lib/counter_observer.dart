330905796c9f62a05fe74619d20ee3a96aa4b65e
Felix Angelov <felangelov@gmail.com>
2021-04-14 21:35:59 -0500

    docs(flutter_counter): fix BlocObserver

--- examples/flutter_counter/lib/counter_observer.dart
+++ examples/flutter_counter/lib/counter_observer.dart
@@ -2,12 +2,12 @@ import 'package:bloc/bloc.dart';
 
 /// {@template counter_observer}
 /// [BlocObserver] for the counter application which
-/// observes all [Bloc] state changes.
+/// observes all state changes.
 /// {@endtemplate}
 class CounterObserver extends BlocObserver {
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    print('${bloc.runtimeType} $transition');
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType} $change');
   }
 }
