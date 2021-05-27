50666f2a5b4c536380ac1a1fe6a531e42481cd2b
Felix Angelov <felangelov@gmail.com>
2021-05-27 12:20:48 -0500

    fix tests

--- examples/flutter_firebase_login/lib/app/bloc_observer.dart
+++ examples/flutter_firebase_login/lib/app/bloc_observer.dart
@@ -1,29 +1,28 @@
-import 'dart:developer';
-
+// ignore_for_file: avoid_print
 import 'package:bloc/bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    log('$event');
+    print(event);
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    log('$error');
+    print(error);
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    log('$change');
+    print(change);
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    log('$transition');
+    print(transition);
   }
 }
