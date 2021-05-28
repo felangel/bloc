36977c61b8b670ba48c9a0d6772b332a0b47aba9
Felix Angelov <felangelov@gmail.com>
2021-05-28 00:32:45 -0500

    refactor(flutter_timer): optimize number of rebuilds

--- examples/flutter_timer/lib/timer/view/timer_page.dart
+++ examples/flutter_timer/lib/timer/view/timer_page.dart
@@ -59,45 +59,49 @@ class Actions extends StatelessWidget {
   const Actions({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final state = context.watch<TimerBloc>().state;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        if (state is TimerInitial) ...[
-          FloatingActionButton(
-            child: Icon(Icons.play_arrow),
-            onPressed: () => context
-                .read<TimerBloc>()
-                .add(TimerStarted(duration: state.duration)),
-          ),
-        ],
-        if (state is TimerRunInProgress) ...[
-          FloatingActionButton(
-            child: Icon(Icons.pause),
-            onPressed: () => context.read<TimerBloc>().add(TimerPaused()),
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.replay),
-            onPressed: () => context.read<TimerBloc>().add(TimerReset()),
-          ),
-        ],
-        if (state is TimerRunPause) ...[
-          FloatingActionButton(
-            child: Icon(Icons.play_arrow),
-            onPressed: () => context.read<TimerBloc>().add(TimerResumed()),
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.replay),
-            onPressed: () => context.read<TimerBloc>().add(TimerReset()),
-          ),
-        ],
-        if (state is TimerRunComplete) ...[
-          FloatingActionButton(
-            child: Icon(Icons.replay),
-            onPressed: () => context.read<TimerBloc>().add(TimerReset()),
-          ),
-        ]
-      ],
+    return BlocBuilder<TimerBloc, TimerState>(
+      buildWhen: (prev, state) => prev.runtimeType != state.runtimeType,
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            if (state is TimerInitial) ...[
+              FloatingActionButton(
+                child: Icon(Icons.play_arrow),
+                onPressed: () => context
+                    .read<TimerBloc>()
+                    .add(TimerStarted(duration: state.duration)),
+              ),
+            ],
+            if (state is TimerRunInProgress) ...[
+              FloatingActionButton(
+                child: Icon(Icons.pause),
+                onPressed: () => context.read<TimerBloc>().add(TimerPaused()),
+              ),
+              FloatingActionButton(
+                child: Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ],
+            if (state is TimerRunPause) ...[
+              FloatingActionButton(
+                child: Icon(Icons.play_arrow),
+                onPressed: () => context.read<TimerBloc>().add(TimerResumed()),
+              ),
+              FloatingActionButton(
+                child: Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ],
+            if (state is TimerRunComplete) ...[
+              FloatingActionButton(
+                child: Icon(Icons.replay),
+                onPressed: () => context.read<TimerBloc>().add(TimerReset()),
+              ),
+            ]
+          ],
+        );
+      },
     );
   }
 }
