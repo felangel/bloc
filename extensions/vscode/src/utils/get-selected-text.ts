1b55d322e87105085d3921bd7b71e26cafce445a
Felix Angelov <felangelov@gmail.com>
2021-06-18 11:20:06 -0500

    fix(vscode): wrap with selection

--- extensions/vscode/src/utils/get-selected-text.ts
+++ extensions/vscode/src/utils/get-selected-text.ts
@@ -1,85 +1,48 @@
-import { TextEditor, Selection } from "vscode";
+import { TextEditor, Selection, Position } from "vscode";
+
+const openBracket = "(";
+const closeBracket = ")";
 
 export const getSelectedText = (editor: TextEditor): Selection => {
-  let offset_l = editor.document.offsetAt(editor.selection.start);
-  let offset_r = editor.document.offsetAt(editor.selection.end) - 1;
-  let text = editor.document.getText();
-  const re = /[^a-zA-Z]/;
-  for (let index = text.length - offset_l; index > 0; index--) {
-    let textOff = text.charAt(offset_l);
-    if (textOff !== "." && re.test(textOff)) {
-      offset_l++;
-      if (/[^A-Z]/.test(text.charAt(offset_l))) {
-        return new Selection(
-          editor.document.positionAt(0),
-          editor.document.positionAt(0)
-        );
-      }
-      let lineText: string = editor.document.lineAt(
-        editor.document.positionAt(offset_l).line
-      ).text;
-      if (
-        lineText.indexOf("class") != -1 ||
-        lineText.indexOf("extends") != -1 ||
-        lineText.indexOf("with") != -1 ||
-        lineText.indexOf("implements") != -1 ||
-        lineText.indexOf("=") != -1
-      ) {
-        return new Selection(
-          editor.document.positionAt(0),
-          editor.document.positionAt(0)
-        );
-      }
-
-      break;
-    } else {
-      offset_l--;
-    }
-  }
-  let l = 0;
-  let r = 0;
-  for (let index = text.length - offset_r; index < text.length; index++) {
-    if (text.charAt(offset_r) === "(") {
-      l++;
-    }
-    if (text.charAt(offset_r) === ")") {
-      r++;
-    }
-
-    if (r > l || index == text.length) {
-      offset_r = 0;
-      offset_l = 0;
-      break;
-    }
-
-    if (l > 0 && l == r) {
-      offset_r++;
-      if (!nextElementIsValid(text, offset_r)) {
-        offset_r = 0;
-        offset_l = 0;
-      }
-      break;
-    }
-    offset_r++;
-  }
-
-  return new Selection(
-    editor.document.positionAt(offset_l),
-    editor.document.positionAt(offset_r)
+  const emptySelection = new Selection(
+    editor.document.positionAt(0),
+    editor.document.positionAt(0)
   );
-};
+  const language = editor.document.languageId;
+  if (language != "dart") return emptySelection;
 
-const nextElementIsValid = (code: string, length: number): Boolean => {
-  for (let index = 0; index < 1000; index++) {
-    const text = code.charAt(length).trim();
-    if (text) {
-      if (/[;),\]]/.test(text)) {
-        return true;
-      } else {
-        return false;
+  const line = editor.document.lineAt(editor.selection.start);
+  const lineText = line.text;
+  const openBracketIndex = line.text.indexOf(
+    "(",
+    editor.selection.anchor.character
+  );
+
+  if (openBracketIndex < 0) return emptySelection;
+
+  let widgetStartIndex = openBracketIndex - 1;
+  for (widgetStartIndex; widgetStartIndex > 0; widgetStartIndex--) {
+    let currentChar = lineText.charAt(widgetStartIndex);
+    if (currentChar === " " || currentChar === openBracket) break;
+  }
+  widgetStartIndex++;
+
+  let bracketCount = 1;
+  for (let l = line.lineNumber; l < editor.document.lineCount; l++) {
+    const currentLine = editor.document.lineAt(l);
+    let c = l === line.lineNumber ? openBracketIndex + 1 : 0;
+    for (c; c < currentLine.text.length; c++) {
+      const currentChar = currentLine.text.charAt(c);
+      if (currentChar === openBracket) bracketCount++;
+      if (currentChar === closeBracket) bracketCount--;
+      if (bracketCount === 0) {
+        return new Selection(
+          new Position(line.lineNumber, widgetStartIndex),
+          new Position(l, c + 1)
+        );
       }
     }
-    length++;
   }
-  return false;
+
+  return emptySelection;
 };
