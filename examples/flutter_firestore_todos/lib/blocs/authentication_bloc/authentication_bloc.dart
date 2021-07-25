1514fcd6f6cccec518eb81d5a407facae4c54aad
Felix Angelov <felangelov@gmail.com>
2021-07-24 19:19:30 -0500

    chore: format

--- examples/flutter_firestore_todos/lib/blocs/authentication_bloc/authentication_bloc.dart
+++ examples/flutter_firestore_todos/lib/blocs/authentication_bloc/authentication_bloc.dart
@@ -12,7 +12,10 @@ class AuthenticationBloc
     on<AppStarted>(_onAppStarted);
   }
 
-  void _onAppStarted(AppStarted event, Emitter<AuthenticationState> emit) async {
+  void _onAppStarted(
+    AppStarted event,
+    Emitter<AuthenticationState> emit,
+  ) async {
     try {
       final isSignedIn = await _userRepository.isAuthenticated();
       if (!isSignedIn) await _userRepository.authenticate();
