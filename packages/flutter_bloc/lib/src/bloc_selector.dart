8c6f42a657ea80e4472f90863ae1871022bf8a19
Felix Angelov <felangelov@gmail.com>
2021-07-25 14:34:36 -0500

    feat(flutter_bloc): BlocSelector

---
+++ packages/flutter_bloc/lib/src/bloc_selector.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// Signature for the `selector` function which
+/// is responsible for returning a selected value, [T], based on [state].
+typedef BlocWidgetSelector<S, T> = T Function(S state);
+
+/// {@template bloc_selector}
+/// [BlocSelector] is analogous to [BlocBuilder] but allows developers to
+/// filter updates by selecting a new value based on the bloc state.
+/// Unnecessary builds are prevented if the selected value does not change.
+///
+/// **Note**: the selected value must be immutable in order for [BlocSelector]
+/// to accurately determine whether [builder] should be called again.
+///
+/// ```dart
+/// BlocSelector<BlocA, BlocAState, SelectedState>(
+///   selector: (state) {
+///     // return selected state based on the provided state.
+///   },
+///   builder: (context, state) {
+///     // return widget here based on the selected state.
+///   },
+/// )
+/// ```
+/// {@endtemplate}
+class BlocSelector<B extends BlocBase<S>, S, T> extends StatefulWidget {
+  /// {@macro bloc_selector}
+  const BlocSelector({
+    Key? key,
+    required this.selector,
+    required this.builder,
+    this.bloc,
+  }) : super(key: key);
+
+  /// The [bloc] that the [BlocSelector] will interact with.
+  /// If omitted, [BlocSelector] will automatically perform a lookup using
+  /// [BlocProvider] and the current [BuildContext].
+  final B? bloc;
+
+  /// The [builder] function which will be invoked
+  /// when the selected state changes.
+  /// The [builder] takes the [BuildContext] and selected `state` and
+  /// must return a widget.
+  /// This is analogous to the [builder] function in [BlocBuilder].
+  final BlocWidgetBuilder<T> builder;
+
+  /// The [selector] function which will be invoked on each widget build
+  /// and is responsible for returning a selected value of type [T] based on
+  /// the current state.
+  final BlocWidgetSelector<S, T> selector;
+
+  @override
+  State<BlocSelector<B, S, T>> createState() => _BlocSelectorState<B, S, T>();
+}
+
+class _BlocSelectorState<B extends BlocBase<S>, S, T>
+    extends State<BlocSelector<B, S, T>> {
+  late B _bloc;
+  late T _state;
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = widget.bloc ?? context.read<B>();
+    _state = widget.selector(_bloc.state);
+  }
+
+  @override
+  void didUpdateWidget(BlocSelector<B, S, T> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final oldBloc = oldWidget.bloc ?? context.read<B>();
+    final currentBloc = widget.bloc ?? oldBloc;
+    if (oldBloc != currentBloc) {
+      _bloc = currentBloc;
+      _state = widget.selector(_bloc.state);
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final bloc = widget.bloc ?? context.read<B>();
+    if (_bloc != bloc) {
+      _bloc = bloc;
+      _state = widget.selector(_bloc.state);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.bloc == null) context.select<B, int>(identityHashCode);
+    return BlocListener<B, S>(
+      bloc: _bloc,
+      listener: (context, state) {
+        final selectedState = widget.selector(state);
+        if (_state != selectedState) setState(() => _state = selectedState);
+      },
+      child: widget.builder(context, _state),
+    );
+  }
+}
