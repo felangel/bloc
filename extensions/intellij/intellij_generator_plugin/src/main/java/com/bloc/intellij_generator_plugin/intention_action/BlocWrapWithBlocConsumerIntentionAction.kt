45258755b0ad57b70b8537cf455231b9d4412e97
Matěj Žídek <zidek.matt@gmail.com>
2021-07-30 04:49:31 +0200

    refactor(intelliJ): migrate to Kotlin (#2616)

---
+++ extensions/intellij/intellij_generator_plugin/src/main/java/com/bloc/intellij_generator_plugin/intention_action/BlocWrapWithBlocConsumerIntentionAction.kt
@@ -0,0 +1,10 @@
+package com.bloc.intellij_generator_plugin.intention_action
+
+class BlocWrapWithBlocConsumerIntentionAction : BlocWrapWithIntentionAction(SnippetType.BlocConsumer) {
+    /**
+     * If this action is applicable, returns the text to be shown in the list of intention actions available.
+     */
+    override fun getText(): String {
+        return "Wrap with BlocConsumer"
+    }
+}
