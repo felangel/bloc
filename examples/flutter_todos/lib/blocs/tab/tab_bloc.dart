591bae65365e2a1ac7c87e532d3bc6e2d3df51bc
Marcos Sevilla <marcossevilla14@gmail.com>
2021-09-06 12:24:23 -0600

    refactor(flutter_todos): migrated TabBloc to v7.2.0

--- examples/flutter_todos/lib/blocs/tab/tab_bloc.dart
+++ examples/flutter_todos/lib/blocs/tab/tab_bloc.dart
@@ -1,15 +1,9 @@
-import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_todos/blocs/tab/tab.dart';
 import 'package:flutter_todos/models/models.dart';
 
 class TabBloc extends Bloc<TabEvent, AppTab> {
-  TabBloc() : super(AppTab.todos);
-
-  @override
-  Stream<AppTab> mapEventToState(TabEvent event) async* {
-    if (event is TabUpdated) {
-      yield event.tab;
-    }
+  TabBloc() : super(AppTab.todos) {
+    on<TabUpdated>((event, emit) => emit(event.tab));
   }
 }
