9abe94acfc9ef464df6bf299b762c194f28b1cdc
Felix Angelov <felangelov@gmail.com>
2021-09-06 22:53:54 -0500

    refactor(github_search): update to v7.2.0

--- examples/github_search/common_github_search/lib/src/github_search_bloc/github_search_bloc.dart
+++ examples/github_search/common_github_search/lib/src/github_search_bloc/github_search_bloc.dart
@@ -1,45 +1,39 @@
-import 'dart:async';
-import 'package:rxdart/rxdart.dart';
 import 'package:bloc/bloc.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 import 'package:common_github_search/common_github_search.dart';
 
+const _duration = const Duration(milliseconds: 300);
+
+EventTransformer<Event> debounce<Event>(Duration duration) {
+  return (events, mapper) => events.debounce(duration).switchMap(mapper);
+}
+
 class GithubSearchBloc extends Bloc<GithubSearchEvent, GithubSearchState> {
   GithubSearchBloc({required this.githubRepository})
-      : super(SearchStateEmpty());
+      : super(SearchStateEmpty()) {
+    on<TextChanged>(_onTextChanged, transformer: debounce(_duration));
+  }
 
   final GithubRepository githubRepository;
 
-  @override
-  Stream<Transition<GithubSearchEvent, GithubSearchState>> transformEvents(
-    Stream<GithubSearchEvent> events,
-    Stream<Transition<GithubSearchEvent, GithubSearchState>> Function(
-      GithubSearchEvent event,
-    )
-        transitionFn,
-  ) {
-    return events
-        .debounceTime(const Duration(milliseconds: 300))
-        .switchMap(transitionFn);
-  }
+  void _onTextChanged(
+    TextChanged event,
+    Emitter<GithubSearchState> emit,
+  ) async {
+    final searchTerm = event.text;
 
-  @override
-  Stream<GithubSearchState> mapEventToState(GithubSearchEvent event) async* {
-    if (event is TextChanged) {
-      final searchTerm = event.text;
-      if (searchTerm.isEmpty) {
-        yield SearchStateEmpty();
-      } else {
-        yield SearchStateLoading();
-        try {
-          final results = await githubRepository.search(searchTerm);
-          yield SearchStateSuccess(results.items);
-        } catch (error) {
-          yield error is SearchResultError
-              ? SearchStateError(error.message)
-              : SearchStateError('something went wrong');
-        }
-      }
+    if (searchTerm.isEmpty) return emit(SearchStateEmpty());
+
+    emit(SearchStateLoading());
+
+    try {
+      final results = await githubRepository.search(searchTerm);
+      emit(SearchStateSuccess(results.items));
+    } catch (error) {
+      emit(error is SearchResultError
+          ? SearchStateError(error.message)
+          : SearchStateError('something went wrong'));
     }
   }
 }
