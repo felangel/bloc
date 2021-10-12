148067671c19ad9d08a23f233735e8653623f19c
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2021-10-12 13:02:31 +0200

    remove old examples

--- examples/_old_flutter_firestore_todos/user_repository/lib/user_repository.dart
+++
@@ -1,4 +0,0 @@
-library user_repository;
-
-export 'src/firebase_user_repository.dart';
-export 'src/user_repository.dart';
--- examples/_old_flutter_todos/lib/blocs/todos/todos.dart
+++
@@ -1,3 +0,0 @@
-export './todos_bloc.dart';
-export './todos_event.dart';
-export './todos_state.dart';
