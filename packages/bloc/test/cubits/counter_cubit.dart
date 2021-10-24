61e3880252b666977a1823c9689eefa47c2624ec
Felix Angelov <felangelov@gmail.com>
2021-10-24 18:29:16 -0500

    fix(bloc): update cubit addError test

--- packages/bloc/test/cubits/counter_cubit.dart
+++ packages/bloc/test/cubits/counter_cubit.dart
@@ -1,9 +1,10 @@
 import 'package:bloc/bloc.dart';
 
 class CounterCubit extends Cubit<int> {
-  CounterCubit({this.onChangeCallback}) : super(0);
+  CounterCubit({this.onChangeCallback, this.onErrorCallback}) : super(0);
 
   final void Function(Change<int>)? onChangeCallback;
+  final void Function(Object error, StackTrace stackTrace)? onErrorCallback;
 
   void increment() => emit(state + 1);
   void decrement() => emit(state - 1);
@@ -12,5 +13,11 @@ class CounterCubit extends Cubit<int> {
   void onChange(Change<int> change) {
     super.onChange(change);
     onChangeCallback?.call(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    onErrorCallback?.call(error, stackTrace);
+    super.onError(error, stackTrace);
   }
 }
