370d6eafcd3eb915592e8e4f0de0a962b7a993d6
Felix Angelov <felangelov@gmail.com>
2021-11-23 17:15:56 -0600

    chore(flutter_weather): use flutter_services_binding (#3001)

--- examples/flutter_weather/lib/main.dart
+++ examples/flutter_weather/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_services_binding/flutter_services_binding.dart';
 import 'package:flutter_weather/app.dart';
 import 'package:flutter_weather/weather_bloc_observer.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -7,7 +8,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:weather_repository/weather_repository.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  FlutterServicesBinding.ensureInitialized();
   final storage = await HydratedStorage.build(
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
