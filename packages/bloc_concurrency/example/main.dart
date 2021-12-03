d743a2d8784efe53b3d583c6ead0ec68424729e5
Felix Angelov <felangelov@gmail.com>
2021-12-02 22:38:21 -0600

    Apply suggestions from code review

--- packages/bloc_concurrency/example/main.dart
+++ packages/bloc_concurrency/example/main.dart
@@ -39,7 +39,7 @@ class CounterIncrementPressed extends CounterEvent {}
 class CounterBloc extends Bloc<CounterEvent, int> {
   /// The initial state of the `CounterBloc` is 0.
   CounterBloc() : super(0) {
-    /// When an `CounterIncrementPressed` event is added,
+    /// When a `CounterIncrementPressed` event is added,
     /// the current `state` of the bloc is accessed via the `state` property
     /// and a new state is emitted via `emit`.
     on<CounterIncrementPressed>(
