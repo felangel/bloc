ca6f078351f1347cf78c4f69eec5d5742060b383
Felix Angelov <felangelov@gmail.com>
2021-12-02 22:33:19 -0600

    docs(bloc): update example event names to be consistent

--- packages/bloc/example/main.dart
+++ packages/bloc/example/main.dart
@@ -76,7 +76,7 @@ Future<void> blocMain() async {
   print(bloc.state);
 
   /// Interact with the `bloc` to trigger `state` changes.
-  bloc.add(CounterIncrementRequested());
+  bloc.add(CounterIncrementPressed());
 
   /// Wait for next iteration of the event-loop
   /// to ensure event has been processed.
@@ -104,15 +104,15 @@ class CounterCubit extends Cubit<int> {
 abstract class CounterEvent {}
 
 /// Notifies bloc to increment state.
-class CounterIncrementRequested extends CounterEvent {}
+class CounterIncrementPressed extends CounterEvent {}
 
 /// A `CounterBloc` which handles converting `CounterEvent`s into `int`s.
 class CounterBloc extends Bloc<CounterEvent, int> {
   /// The initial state of the `CounterBloc` is 0.
   CounterBloc() : super(0) {
-    /// When a `CounterIncrementRequested` event is added,
+    /// When a `CounterIncrementPressed` event is added,
     /// the current `state` of the bloc is accessed via the `state` property
     /// and a new state is emitted via `emit`.
-    on<CounterIncrementRequested>((event, emit) => emit(state + 1));
+    on<CounterIncrementPressed>((event, emit) => emit(state + 1));
   }
 }
