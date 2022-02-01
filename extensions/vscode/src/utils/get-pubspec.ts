b1e26b2f500a9a919f770d4427d88cb3af084671
Alejandro Santiago <dev@alestiago.com>
2022-02-01 01:20:16 +0000

    feat(vscode): determine versions via pubspec.lock (#3175)

--- extensions/vscode/src/utils/get-pubspec.ts
+++ extensions/vscode/src/utils/get-pubspec.ts
@@ -1,12 +1,21 @@
 import * as yaml from "js-yaml";
-import { getPubspecPath } from "./get-pubspec-path";
+import { getPubspecPath, getPubspecLockPath } from "./get-pubspec-path";
 import { workspace, Uri } from "vscode";
 
 export async function getPubspec(): Promise<Record<string, any> | undefined> {
   const pubspecPath = getPubspecPath();
-  if (pubspecPath) {
+  return getYAMLFileContent(pubspecPath);
+}
+
+export async function getPubspecLock(): Promise<Record<string, any> | undefined> {
+  const pubspecLockPath = getPubspecLockPath();
+  return getYAMLFileContent(pubspecLockPath);
+}
+
+async function getYAMLFileContent(path: string | undefined): Promise<Record<string, any> | undefined> {
+  if (path) {
     try {
-      let content = await workspace.fs.readFile(Uri.file(pubspecPath));
+      let content = await workspace.fs.readFile(Uri.file(path));
       return yaml.load(content.toString());
     } catch (_) {}
   }
