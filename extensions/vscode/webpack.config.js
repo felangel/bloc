a7f37738d683e9b26e332ad5a6f13b7117571b7e
Srishan Shakya <csunshakya@gmail.com>
2022-03-20 15:15:00 +0545

    change webpack output.path to dist, package main to ./dist/extension & vscodeignore out/

--- extensions/vscode/webpack.config.js
+++ extensions/vscode/webpack.config.js
@@ -8,8 +8,8 @@ const config = {
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
-    // the bundle is stored in the 'out' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, 'out'),
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
     filename: 'extension.js',
     libraryTarget: 'commonjs2',
     devtoolModuleFilenameTemplate: '../[resource-path]'
