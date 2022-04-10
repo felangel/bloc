cbf1b9db0f1e644dc5b1d3ff2ad76b018fc1d779
Felix Angelov <felangelov@gmail.com>
2022-04-09 19:56:57 -0500

    refactor(bricks): upgrade cubit to shorthand lambda syntax (#3321)

--- bricks/cubit/CHANGELOG.md
+++ bricks/cubit/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1
+
+- refactor: upgrade to shorthand lambda syntax (`mason >= 0.1.0-dev.15`)
+
 # 0.1.0
 
 - feat: initial release with support for basic cubit generation
--- bricks/cubit/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-part '{{#snakeCase}}{{name}}{{/snakeCase}}_state.dart';
-
-class {{#pascalCase}}{{name}}{{/pascalCase}}Cubit extends Cubit<{{#pascalCase}}{{name}}{{/pascalCase}}State> {
-  {{#pascalCase}}{{name}}{{/pascalCase}}Cubit() : super(const {{#pascalCase}}{{name}}{{/pascalCase}}State());
-}
