7ee9686dbf8b2a21e106ca8b643f1c743c13149d
Felix Angelov <felangelov@gmail.com>
2022-06-06 12:38:37 -0500

    chore(flutter_bloc_with_stream): upgrade to Dart 2.17 (#3390)

--- examples/flutter_bloc_with_stream/test/ticker_page_test.dart
+++ examples/flutter_bloc_with_stream/test/ticker_page_test.dart
@@ -39,7 +39,7 @@ void main() {
     });
 
     testWidgets('renders tick count ', (tester) async {
-      final tickCount = 5;
+      const tickCount = 5;
       when(() => tickerBloc.state).thenReturn(TickerTickSuccess(tickCount));
       await tester.pumpTickerPage(tickerBloc);
 
