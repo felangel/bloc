997bb872fe2475d22d61c3d9b64cb00b12cb46b9
Hubert Kuczyński <hubert.kuczynski@verygood.ventures>
2022-07-27 22:58:09 +0200

    chore(flutter_github_search): upgrade to Dart 2.17

--- examples/github_search/common_github_search/lib/common_github_search.dart
+++ examples/github_search/common_github_search/lib/common_github_search.dart
@@ -1,7 +1,7 @@
+export 'src/github_cache.dart';
+export 'src/github_client.dart';
+export 'src/github_repository.dart';
 export 'src/github_search_bloc/github_search_bloc.dart';
 export 'src/github_search_bloc/github_search_event.dart';
 export 'src/github_search_bloc/github_search_state.dart';
-export 'src/github_repository.dart';
-export 'src/github_cache.dart';
-export 'src/github_client.dart';
 export 'src/models/models.dart';
