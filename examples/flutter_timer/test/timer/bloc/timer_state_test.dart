983dac0c70faa55899d8c12b597c43b19a5dd459
Hubert Kuczyński <8619562+hkuczynski@users.noreply.github.com>
2022-07-31 04:53:26 +0200

    chore(flutter_timer): upgrade to Dart 2.17 (#3454)

--- examples/flutter_timer/test/timer/bloc/timer_state_test.dart
+++ examples/flutter_timer/test/timer/bloc/timer_state_test.dart
@@ -1,37 +1,39 @@
-import 'package:flutter_timer/timer/timer.dart';
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_timer/timer/timer.dart';
 
 void main() {
   group('TimerState', () {
     group('TimerInitial', () {
       test('supports value comparison', () {
         expect(
-          const TimerInitial(60),
-          const TimerInitial(60),
+          TimerInitial(60),
+          TimerInitial(60),
         );
       });
     });
     group('TimerRunPause', () {
       test('supports value comparison', () {
         expect(
-          const TimerRunPause(60),
-          const TimerRunPause(60),
+          TimerRunPause(60),
+          TimerRunPause(60),
         );
       });
     });
     group('TimerRunInProgress', () {
       test('supports value comparison', () {
         expect(
-          const TimerRunInProgress(60),
-          const TimerRunInProgress(60),
+          TimerRunInProgress(60),
+          TimerRunInProgress(60),
         );
       });
     });
     group('TimerRunComplete', () {
       test('supports value comparison', () {
         expect(
-          const TimerRunComplete(),
-          const TimerRunComplete(),
+          TimerRunComplete(),
+          TimerRunComplete(),
         );
       });
     });
