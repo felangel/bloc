997cfa8835d5e2378bb770f45d970f457cf664fa
Hubert Kuczyński <8619562+hkuczynski@users.noreply.github.com>
2022-07-31 04:43:51 +0200

    chore(flutter_shopping_cart): upgrade to Dart 2.17 (#3453)

--- examples/flutter_shopping_cart/test/catalog/bloc/catalog_state_test.dart
+++ examples/flutter_shopping_cart/test/catalog/bloc/catalog_state_test.dart
@@ -1,4 +1,5 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors,
+
 import 'package:flutter_shopping_cart/catalog/catalog.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -12,7 +13,7 @@ void main() {
 
     group('CatalogLoaded', () {
       test('supports value comparison', () {
-        final catalog = Catalog(itemNames: ['item #1', 'item #2']);
+        final catalog = Catalog(itemNames: const ['item #1', 'item #2']);
         expect(CatalogLoaded(catalog), CatalogLoaded(catalog));
       });
     });
