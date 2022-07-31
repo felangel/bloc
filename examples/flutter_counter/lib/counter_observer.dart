4e066be329e22673e3fe200874cb9fbee9d399ac
Felix Angelov <felangelov@gmail.com>
2022-07-30 20:29:16 -0500

    fixes

--- examples/flutter_counter/lib/counter_observer.dart
+++ examples/flutter_counter/lib/counter_observer.dart
@@ -6,7 +6,7 @@ import 'package:bloc/bloc.dart';
 /// {@endtemplate}
 class CounterObserver extends BlocObserver {
   @override
-  void onChange(BlocBase bloc, Change change) {
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
     // ignore: avoid_print
     print('${bloc.runtimeType} $change');
--- examples/flutter_counter/test/counter/view/counter_view_test.dart
+++ examples/flutter_counter/test/counter/view/counter_view_test.dart
@@ -4,7 +4,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_counter/counter/counter.dart';
-import 'package:flutter_counter/counter/view/counter_view.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
