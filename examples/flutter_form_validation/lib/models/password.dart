921428c67719404a34f219f452ef1b035cecb8cd
Hubert Kuczyński <8619562+hkuczynski@users.noreply.github.com>
2022-07-31 03:56:46 +0200

    chore(flutter_form_validation): upgrade to Dart 2.17 (#3449)

--- examples/flutter_form_validation/lib/models/password.dart
+++ examples/flutter_form_validation/lib/models/password.dart
@@ -3,8 +3,8 @@ import 'package:formz/formz.dart';
 enum PasswordValidationError { invalid }
 
 class Password extends FormzInput<String, PasswordValidationError> {
-  const Password.pure([String value = '']) : super.pure(value);
-  const Password.dirty([String value = '']) : super.dirty(value);
+  const Password.pure([super.value = '']) : super.pure();
+  const Password.dirty([super.value = '']) : super.dirty();
 
   static final _passwordRegex =
       RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$');
