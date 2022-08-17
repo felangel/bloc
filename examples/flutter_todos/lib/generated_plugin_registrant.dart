7f50f296222859d8cb28fcb3a980060b6ebd6a47
Maxim Veksler <maxim@vekslers.org>
2022-08-17 15:24:32 +0300

    Updating firebase dependencies

--- examples/flutter_todos/lib/generated_plugin_registrant.dart
+++ examples/flutter_todos/lib/generated_plugin_registrant.dart
@@ -6,12 +6,16 @@
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  FirebaseFirestoreWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
