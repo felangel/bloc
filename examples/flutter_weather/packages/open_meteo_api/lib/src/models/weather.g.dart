f67a3f31e6799abc2283d5e4c2fb5f27869ca32d
Felix Angelov <felangelov@gmail.com>
2022-09-11 22:20:39 -0500

    fix(flutter_weather): use open-meteo weather api (#3529)

---
+++ examples/flutter_weather/packages/open_meteo_api/lib/src/models/weather.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// ignore_for_file: implicit_dynamic_parameter
+
+part of 'weather.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Weather _$WeatherFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Weather',
+      json,
+      ($checkedConvert) {
+        final val = Weather(
+          temperature:
+              $checkedConvert('temperature', (v) => (v as num).toDouble()),
+          weatherCode:
+              $checkedConvert('weathercode', (v) => (v as num).toDouble()),
+        );
+        return val;
+      },
+      fieldKeyMap: const {'weatherCode': 'weathercode'},
+    );
