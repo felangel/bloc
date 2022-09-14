b92f20fb398243291f14c63525092fbb55376377
Martin Neff <martin.neff@approppo.ch>
2022-09-14 10:47:05 +0200

    Remove useless javadoc from BlocTemplateType enum
    Co-authored-by: Felix Angelov <felangelov@gmail.com>

--- extensions/intellij/intellij_generator_plugin/src/main/java/com/bloc/intellij_generator_plugin/action/BlocStatePackage.java
+++ extensions/intellij/intellij_generator_plugin/src/main/java/com/bloc/intellij_generator_plugin/action/BlocStatePackage.java
@@ -1,9 +1,5 @@
 package com.bloc.intellij_generator_plugin.action;
 
-/**
- * @author Martin Neff, approppo GmbH (martin.neff@approppo.ch)
- * @since 13.09.22
- */
 public enum BlocTemplateType {
     NONE,
     EQUATABLE,
