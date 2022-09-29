36eae6ea4939025c948f2b5afae0ed7cac7fce15
Felix Angelov <felangelov@gmail.com>
2022-09-29 10:18:51 -0500

    revert

--- packages/bloc_tools/test/ensure_build_test.dart
+++ packages/bloc_tools/test/ensure_build_test.dart
@@ -3,5 +3,7 @@ import 'package:build_verify/build_verify.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ensure_build', expectBuildClean);
+  test('ensure_build', () {
+    expectBuildClean(packageRelativeDirectory: 'packages/bloc_tools');
+  });
 }
