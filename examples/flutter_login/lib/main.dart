1643dc4c72d05e59abea2a8d9e5180bf5e34bd5a
Benson Arafat <benpaul320@gmail.com>
2023-01-16 21:40:48 +0100

    refactor(flutter_login): dispose authentication repository (#3681)

--- examples/flutter_login/lib/main.dart
+++ examples/flutter_login/lib/main.dart
@@ -1,13 +1,4 @@
-import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_login/app.dart';
-import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(
-    App(
-      authenticationRepository: AuthenticationRepository(),
-      userRepository: UserRepository(),
-    ),
-  );
-}
+void main() => runApp(const App());
