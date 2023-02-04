f019ee4e7ac8be5e154e32c85577cf64c32bbd68
vladimir.koltunov <progrand@gmail.com>
2023-02-04 18:26:57 +0300

    build_runner

--- packages/hydrated_bloc/test/cubits/freezed_cubit.freezed.dart
+++ packages/hydrated_bloc/test/cubits/freezed_cubit.freezed.dart
@@ -1,6 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'freezed_cubit.dart';
 
@@ -11,30 +12,11 @@ part of 'freezed_cubit.dart';
 T _$identity<T>(T value) => value;
 
 final _privateConstructorUsedError = UnsupportedError(
-    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
 
 Question _$QuestionFromJson(Map<String, dynamic> json) {
   return _Question.fromJson(json);
 }
-
-/// @nodoc
-class _$QuestionTearOff {
-  const _$QuestionTearOff();
-
-  _Question call({int? id, String? question}) {
-    return _Question(
-      id: id,
-      question: question,
-    );
-  }
-
-  Question fromJson(Map<String, Object?> json) {
-    return Question.fromJson(json);
-  }
-}
-
-/// @nodoc
-const $Question = _$QuestionTearOff();
 
 /// @nodoc
 mixin _$Question {
@@ -50,64 +32,70 @@ mixin _$Question {
 /// @nodoc
 abstract class $QuestionCopyWith<$Res> {
   factory $QuestionCopyWith(Question value, $Res Function(Question) then) =
-      _$QuestionCopyWithImpl<$Res>;
+      _$QuestionCopyWithImpl<$Res, Question>;
+  @useResult
   $Res call({int? id, String? question});
 }
 
 /// @nodoc
-class _$QuestionCopyWithImpl<$Res> implements $QuestionCopyWith<$Res> {
+class _$QuestionCopyWithImpl<$Res, $Val extends Question>
+    implements $QuestionCopyWith<$Res> {
   _$QuestionCopyWithImpl(this._value, this._then);
 
-  final Question _value;
   // ignore: unused_field
-  final $Res Function(Question) _then;
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
 
+  @pragma('vm:prefer-inline')
   @override
   $Res call({
     Object? id = freezed,
     Object? question = freezed,
   }) {
     return _then(_value.copyWith(
-      id: id == freezed
+      id: freezed == id
           ? _value.id
           : id // ignore: cast_nullable_to_non_nullable
               as int?,
-      question: question == freezed
+      question: freezed == question
           ? _value.question
           : question // ignore: cast_nullable_to_non_nullable
               as String?,
-    ));
+    ) as $Val);
   }
 }
 
 /// @nodoc
-abstract class _$QuestionCopyWith<$Res> implements $QuestionCopyWith<$Res> {
-  factory _$QuestionCopyWith(_Question value, $Res Function(_Question) then) =
-      __$QuestionCopyWithImpl<$Res>;
+abstract class _$$_QuestionCopyWith<$Res> implements $QuestionCopyWith<$Res> {
+  factory _$$_QuestionCopyWith(
+          _$_Question value, $Res Function(_$_Question) then) =
+      __$$_QuestionCopyWithImpl<$Res>;
   @override
+  @useResult
   $Res call({int? id, String? question});
 }
 
 /// @nodoc
-class __$QuestionCopyWithImpl<$Res> extends _$QuestionCopyWithImpl<$Res>
-    implements _$QuestionCopyWith<$Res> {
-  __$QuestionCopyWithImpl(_Question _value, $Res Function(_Question) _then)
-      : super(_value, (v) => _then(v as _Question));
+class __$$_QuestionCopyWithImpl<$Res>
+    extends _$QuestionCopyWithImpl<$Res, _$_Question>
+    implements _$$_QuestionCopyWith<$Res> {
+  __$$_QuestionCopyWithImpl(
+      _$_Question _value, $Res Function(_$_Question) _then)
+      : super(_value, _then);
 
-  @override
-  _Question get _value => super._value as _Question;
-
+  @pragma('vm:prefer-inline')
   @override
   $Res call({
     Object? id = freezed,
     Object? question = freezed,
   }) {
-    return _then(_Question(
-      id: id == freezed
+    return _then(_$_Question(
+      id: freezed == id
           ? _value.id
           : id // ignore: cast_nullable_to_non_nullable
               as int?,
-      question: question == freezed
+      question: freezed == question
           ? _value.question
           : question // ignore: cast_nullable_to_non_nullable
               as String?,
@@ -137,28 +125,33 @@ class _$_Question implements _Question {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _Question &&
+            other is _$_Question &&
             (identical(other.id, id) || other.id == id) &&
             (identical(other.question, question) ||
                 other.question == question));
   }
 
+  @JsonKey(ignore: true)
   @override
   int get hashCode => Object.hash(runtimeType, id, question);
 
   @JsonKey(ignore: true)
   @override
-  _$QuestionCopyWith<_Question> get copyWith =>
-      __$QuestionCopyWithImpl<_Question>(this, _$identity);
+  @pragma('vm:prefer-inline')
+  _$$_QuestionCopyWith<_$_Question> get copyWith =>
+      __$$_QuestionCopyWithImpl<_$_Question>(this, _$identity);
 
   @override
   Map<String, dynamic> toJson() {
-    return _$$_QuestionToJson(this);
+    return _$$_QuestionToJson(
+      this,
+    );
   }
 }
 
 abstract class _Question implements Question {
-  const factory _Question({int? id, String? question}) = _$_Question;
+  const factory _Question({final int? id, final String? question}) =
+      _$_Question;
 
   factory _Question.fromJson(Map<String, dynamic> json) = _$_Question.fromJson;
 
@@ -168,33 +161,13 @@ abstract class _Question implements Question {
   String? get question;
   @override
   @JsonKey(ignore: true)
-  _$QuestionCopyWith<_Question> get copyWith =>
+  _$$_QuestionCopyWith<_$_Question> get copyWith =>
       throw _privateConstructorUsedError;
 }
 
 Tree _$TreeFromJson(Map<String, dynamic> json) {
   return _QTree.fromJson(json);
 }
-
-/// @nodoc
-class _$TreeTearOff {
-  const _$TreeTearOff();
-
-  _QTree call({Question? question, Tree? left, Tree? right}) {
-    return _QTree(
-      question: question,
-      left: left,
-      right: right,
-    );
-  }
-
-  Tree fromJson(Map<String, Object?> json) {
-    return Tree.fromJson(json);
-  }
-}
-
-/// @nodoc
-const $Tree = _$TreeTearOff();
 
 /// @nodoc
 mixin _$Tree {
@@ -210,7 +183,8 @@ mixin _$Tree {
 /// @nodoc
 abstract class $TreeCopyWith<$Res> {
   factory $TreeCopyWith(Tree value, $Res Function(Tree) then) =
-      _$TreeCopyWithImpl<$Res>;
+      _$TreeCopyWithImpl<$Res, Tree>;
+  @useResult
   $Res call({Question? question, Tree? left, Tree? right});
 
   $QuestionCopyWith<$Res>? get question;
@@ -219,13 +193,16 @@ abstract class $TreeCopyWith<$Res> {
 }
 
 /// @nodoc
-class _$TreeCopyWithImpl<$Res> implements $TreeCopyWith<$Res> {
+class _$TreeCopyWithImpl<$Res, $Val extends Tree>
+    implements $TreeCopyWith<$Res> {
   _$TreeCopyWithImpl(this._value, this._then);
 
-  final Tree _value;
   // ignore: unused_field
-  final $Res Function(Tree) _then;
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
 
+  @pragma('vm:prefer-inline')
   @override
   $Res call({
     Object? question = freezed,
@@ -233,60 +210,64 @@ class _$TreeCopyWithImpl<$Res> implements $TreeCopyWith<$Res> {
     Object? right = freezed,
   }) {
     return _then(_value.copyWith(
-      question: question == freezed
+      question: freezed == question
           ? _value.question
           : question // ignore: cast_nullable_to_non_nullable
               as Question?,
-      left: left == freezed
+      left: freezed == left
           ? _value.left
           : left // ignore: cast_nullable_to_non_nullable
               as Tree?,
-      right: right == freezed
+      right: freezed == right
           ? _value.right
           : right // ignore: cast_nullable_to_non_nullable
               as Tree?,
-    ));
+    ) as $Val);
   }
 
   @override
+  @pragma('vm:prefer-inline')
   $QuestionCopyWith<$Res>? get question {
     if (_value.question == null) {
       return null;
     }
 
     return $QuestionCopyWith<$Res>(_value.question!, (value) {
-      return _then(_value.copyWith(question: value));
+      return _then(_value.copyWith(question: value) as $Val);
     });
   }
 
   @override
+  @pragma('vm:prefer-inline')
   $TreeCopyWith<$Res>? get left {
     if (_value.left == null) {
       return null;
     }
 
     return $TreeCopyWith<$Res>(_value.left!, (value) {
-      return _then(_value.copyWith(left: value));
+      return _then(_value.copyWith(left: value) as $Val);
     });
   }
 
   @override
+  @pragma('vm:prefer-inline')
   $TreeCopyWith<$Res>? get right {
     if (_value.right == null) {
       return null;
     }
 
     return $TreeCopyWith<$Res>(_value.right!, (value) {
-      return _then(_value.copyWith(right: value));
+      return _then(_value.copyWith(right: value) as $Val);
     });
   }
 }
 
 /// @nodoc
-abstract class _$QTreeCopyWith<$Res> implements $TreeCopyWith<$Res> {
-  factory _$QTreeCopyWith(_QTree value, $Res Function(_QTree) then) =
-      __$QTreeCopyWithImpl<$Res>;
+abstract class _$$_QTreeCopyWith<$Res> implements $TreeCopyWith<$Res> {
+  factory _$$_QTreeCopyWith(_$_QTree value, $Res Function(_$_QTree) then) =
+      __$$_QTreeCopyWithImpl<$Res>;
   @override
+  @useResult
   $Res call({Question? question, Tree? left, Tree? right});
 
   @override
@@ -298,30 +279,28 @@ abstract class _$QTreeCopyWith<$Res> implements $TreeCopyWith<$Res> {
 }
 
 /// @nodoc
-class __$QTreeCopyWithImpl<$Res> extends _$TreeCopyWithImpl<$Res>
-    implements _$QTreeCopyWith<$Res> {
-  __$QTreeCopyWithImpl(_QTree _value, $Res Function(_QTree) _then)
-      : super(_value, (v) => _then(v as _QTree));
+class __$$_QTreeCopyWithImpl<$Res> extends _$TreeCopyWithImpl<$Res, _$_QTree>
+    implements _$$_QTreeCopyWith<$Res> {
+  __$$_QTreeCopyWithImpl(_$_QTree _value, $Res Function(_$_QTree) _then)
+      : super(_value, _then);
 
-  @override
-  _QTree get _value => super._value as _QTree;
-
+  @pragma('vm:prefer-inline')
   @override
   $Res call({
     Object? question = freezed,
     Object? left = freezed,
     Object? right = freezed,
   }) {
-    return _then(_QTree(
-      question: question == freezed
+    return _then(_$_QTree(
+      question: freezed == question
           ? _value.question
           : question // ignore: cast_nullable_to_non_nullable
               as Question?,
-      left: left == freezed
+      left: freezed == left
           ? _value.left
           : left // ignore: cast_nullable_to_non_nullable
               as Tree?,
-      right: right == freezed
+      right: freezed == right
           ? _value.right
           : right // ignore: cast_nullable_to_non_nullable
               as Tree?,
@@ -353,30 +332,36 @@ class _$_QTree implements _QTree {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
-            other is _QTree &&
+            other is _$_QTree &&
             (identical(other.question, question) ||
                 other.question == question) &&
             (identical(other.left, left) || other.left == left) &&
             (identical(other.right, right) || other.right == right));
   }
 
+  @JsonKey(ignore: true)
   @override
   int get hashCode => Object.hash(runtimeType, question, left, right);
 
   @JsonKey(ignore: true)
   @override
-  _$QTreeCopyWith<_QTree> get copyWith =>
-      __$QTreeCopyWithImpl<_QTree>(this, _$identity);
+  @pragma('vm:prefer-inline')
+  _$$_QTreeCopyWith<_$_QTree> get copyWith =>
+      __$$_QTreeCopyWithImpl<_$_QTree>(this, _$identity);
 
   @override
   Map<String, dynamic> toJson() {
-    return _$$_QTreeToJson(this);
+    return _$$_QTreeToJson(
+      this,
+    );
   }
 }
 
 abstract class _QTree implements Tree {
-  const factory _QTree({Question? question, Tree? left, Tree? right}) =
-      _$_QTree;
+  const factory _QTree(
+      {final Question? question,
+      final Tree? left,
+      final Tree? right}) = _$_QTree;
 
   factory _QTree.fromJson(Map<String, dynamic> json) = _$_QTree.fromJson;
 
@@ -388,5 +373,6 @@ abstract class _QTree implements Tree {
   Tree? get right;
   @override
   @JsonKey(ignore: true)
-  _$QTreeCopyWith<_QTree> get copyWith => throw _privateConstructorUsedError;
+  _$$_QTreeCopyWith<_$_QTree> get copyWith =>
+      throw _privateConstructorUsedError;
 }
