04580459c03459e0ff0111a8db04257b1cf82b64
Felix Angelov <felangelov@gmail.com>
2023-02-05 23:12:44 -0600

    Update packages/flutter_bloc/example/lib/main.dart

--- packages/flutter_bloc/example/lib/main.dart
+++ packages/flutter_bloc/example/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 }
 
 /// {@template app_bloc_observer}
-
 /// Custom [BlocObserver] that observes all bloc and cubit state changes.
 /// {@endtemplate}
 class AppBlocObserver extends BlocObserver {
