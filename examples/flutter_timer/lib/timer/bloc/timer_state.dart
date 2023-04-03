02721601afa35dcb1573f9f30e920c2caf1f1048
Ahmed Abdelhady <109347761+AhmedAbdelhady24@users.noreply.github.com>
2023-04-03 17:33:32 +0200

    update timer_state.dart
    the duration value would never be updated that way as it would always get either 60 when the timer gets initiated or 0 when it is completed but since it should be updated accordingly when TimerRunInProgress gets called and when TimerRunPause gets called so we need to pass the duration argument to the parent constructor to update its value.

--- examples/flutter_timer/lib/timer/bloc/timer_state.dart
+++ examples/flutter_timer/lib/timer/bloc/timer_state.dart
@@ -1,32 +1,30 @@
 part of 'timer_bloc.dart';
 
 abstract class TimerState extends Equatable {
-  const TimerState(this.duration);
   final int duration;
+
+  const TimerState(this.duration);
 
   @override
   List<Object> get props => [duration];
 }
 
 class TimerInitial extends TimerState {
-  const TimerInitial(super.duration);
-
+  const TimerInitial(int duration) : super(duration);
   @override
   String toString() => 'TimerInitial { duration: $duration }';
 }
 
-class TimerRunPause extends TimerState {
-  const TimerRunPause(super.duration);
-
-  @override
-  String toString() => 'TimerRunPause { duration: $duration }';
-}
-
 class TimerRunInProgress extends TimerState {
-  const TimerRunInProgress(super.duration);
-
+  const TimerRunInProgress(int duration) : super(duration);
   @override
   String toString() => 'TimerRunInProgress { duration: $duration }';
+}
+
+class TimerRunPause extends TimerState {
+  const TimerRunPause(int duration) : super(duration);
+  @override
+  String toString() => 'TimerRunPause { duration: $duration }';
 }
 
 class TimerRunComplete extends TimerState {
