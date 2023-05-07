9443aa33cfb22c53ba260d266905da5a30aa2b2b
Felix Angelov <felangelov@gmail.com>
2023-05-07 18:07:10 -0500

    feat(github_search): upgrade to Dart 3

--- examples/github_search/common_github_search/lib/src/github_search_bloc/github_search_state.dart
+++ examples/github_search/common_github_search/lib/src/github_search_bloc/github_search_state.dart
@@ -1,18 +1,18 @@
 import 'package:common_github_search/common_github_search.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class GithubSearchState extends Equatable {
+sealed class GithubSearchState extends Equatable {
   const GithubSearchState();
 
   @override
   List<Object> get props => [];
 }
 
-class SearchStateEmpty extends GithubSearchState {}
+final class SearchStateEmpty extends GithubSearchState {}
 
-class SearchStateLoading extends GithubSearchState {}
+final class SearchStateLoading extends GithubSearchState {}
 
-class SearchStateSuccess extends GithubSearchState {
+final class SearchStateSuccess extends GithubSearchState {
   const SearchStateSuccess(this.items);
 
   final List<SearchResultItem> items;
@@ -24,7 +24,7 @@ class SearchStateSuccess extends GithubSearchState {
   String toString() => 'SearchStateSuccess { items: ${items.length} }';
 }
 
-class SearchStateError extends GithubSearchState {
+final class SearchStateError extends GithubSearchState {
   const SearchStateError(this.error);
 
   final String error;
