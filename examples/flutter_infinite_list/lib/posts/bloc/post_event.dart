b64f579d3543cc781dc27379fcb0c78e85061479
Felix Angelov <felangelov@gmail.com>
2023-05-07 17:11:25 -0500

    feat(flutter_infinite_list): upgrade to Dart 3

--- examples/flutter_infinite_list/lib/posts/bloc/post_event.dart
+++ examples/flutter_infinite_list/lib/posts/bloc/post_event.dart
@@ -1,8 +1,8 @@
 part of 'post_bloc.dart';
 
-abstract class PostEvent extends Equatable {
+sealed class PostEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
 
-class PostFetched extends PostEvent {}
+final class PostFetched extends PostEvent {}
