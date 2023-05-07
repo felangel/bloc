1fa1250d85e628cfb338fe78f157aaa979f1730f
Felix Angelov <felangelov@gmail.com>
2023-05-07 16:42:37 -0500

    feat(flutter_complex_list): upgrade to Dart 3

--- examples/flutter_complex_list/lib/app.dart
+++ examples/flutter_complex_list/lib/app.dart
@@ -4,7 +4,7 @@ import 'package:flutter_complex_list/complex_list/complex_list.dart';
 import 'package:flutter_complex_list/repository.dart';
 
 class App extends MaterialApp {
-  App({super.key, required Repository repository})
+  App({required Repository repository, super.key})
       : super(
           home: RepositoryProvider.value(
             value: repository,
