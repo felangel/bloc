6eb645e8f84dca8904181f649c6ea6dd57996add
Felix Angelov <felangelov@gmail.com>
2023-05-07 17:38:07 -0500

    feat(flutter_weather): upgrade to Dart 3

--- examples/flutter_weather/test/weather/cubit/weather_cubit_test.dart
+++ examples/flutter_weather/test/weather/cubit/weather_cubit_test.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_weather/weather/weather.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:test/test.dart';
 import 'package:weather_repository/weather_repository.dart'
     as weather_repository;
 
