b6de9852c8cb44cd19d87f089540d56927776627
Felix Angelov <felangelov@gmail.com>
2023-05-07 17:29:49 -0500

    feat(flutter_timer): upgrade to Dart 3

--- examples/flutter_timer/lib/timer/view/timer_page.dart
+++ examples/flutter_timer/lib/timer/view/timer_page.dart
@@ -22,12 +22,12 @@ class TimerView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Flutter Timer')),
-      body: Stack(
+      body: const Stack(
         children: [
-          const Background(),
+          Background(),
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
+            children: <Widget>[
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 100),
                 child: Center(child: TimerText()),
