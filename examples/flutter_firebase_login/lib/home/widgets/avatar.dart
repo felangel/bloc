8f967d15e383623c68d27269aadcc42f84ec3b0d
adil el khallouki <91480638+devLegende@users.noreply.github.com>
2023-06-10 15:21:57 +0100

    Update avatar.dart
     i gess we can use null assertion operator(!), so this variable photo in build methode became useless

--- examples/flutter_firebase_login/lib/home/widgets/avatar.dart
+++ examples/flutter_firebase_login/lib/home/widgets/avatar.dart
@@ -9,10 +9,10 @@ class Avatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final photo = this.photo;
+    //final photo = this.photo; i gess we can use null assertion operator(!), so this variable became useless
     return CircleAvatar(
       radius: _avatarSize,
-      backgroundImage: photo != null ? NetworkImage(photo) : null,
+      backgroundImage: photo != null ? NetworkImage(photo!) : null,
       child: photo == null
           ? const Icon(Icons.person_outline, size: _avatarSize)
           : null,
