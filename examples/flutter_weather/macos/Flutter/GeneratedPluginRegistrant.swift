7cb7b5afed3f0a06eb9728728e5b7a62909f3c44
Felix Angelov <felangelov@gmail.com>
2023-08-01 21:19:51 -0500

    chore(deps): upgrade to `package:mocktail` v1.0.0 (#3919)

--- examples/flutter_weather/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/flutter_weather/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_foundation
+import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
