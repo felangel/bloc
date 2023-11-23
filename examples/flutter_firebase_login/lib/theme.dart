9bb2eeb053856a9e7f8a7396ef59520f3025749b
Felix Angelov <felangelov@gmail.com>
2023-11-23 11:54:33 -0600

    chore(flutter_firebase_login): upgrade deps (#4009)

--- examples/flutter_firebase_login/lib/theme.dart
+++ examples/flutter_firebase_login/lib/theme.dart
@@ -3,11 +3,15 @@ import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
   textTheme: GoogleFonts.openSansTextTheme(),
-  primaryColorDark: const Color(0xFF0097A7),
-  primaryColorLight: const Color(0xFFB2EBF2),
-  primaryColor: const Color(0xFF00BCD4),
-  colorScheme: const ColorScheme.light(secondary: Color(0xFF009688)),
-  scaffoldBackgroundColor: const Color(0xFFE0F2F1),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color.fromARGB(255, 113, 243, 230),
+    elevation: 4,
+  ),
+  colorScheme: const ColorScheme.light(
+    primary: Color(0xFF0097A7),
+    secondary: Color(0xFF009688),
+    background: Color(0xFFE0F2F1),
+  ),
   inputDecorationTheme: InputDecorationTheme(
     border: OutlineInputBorder(
       borderRadius: BorderRadius.circular(8),
