6ef8e6d78ae25d328908bee641f4fe75eee4e042
Felix Angelov <felangelov@gmail.com>
2024-03-10 15:48:18 -0500

    fix(vscode): wrap with preserves escaping (#4093)

--- extensions/vscode/src/utils/wrap-with.ts
+++ extensions/vscode/src/utils/wrap-with.ts
@@ -1,16 +1,17 @@
 import { commands, SnippetString, window } from "vscode";
 import { getSelectedText } from "../utils";
 
-const interpolatedVarRegExp = new RegExp("\\$", "g");
+const interpolatedVarRegExp = /[$]/g;
+const escapedCharacterRegExp = /[\\]/g;
 
 export const wrapWith = async (snippet: (widget: string) => string) => {
   let editor = window.activeTextEditor;
   if (!editor) return;
   const selection = getSelectedText(editor);
-  const widget = editor.document.getText(selection).replace(
-    interpolatedVarRegExp,
-    "\\$",
-  );
+  const widget = editor.document
+    .getText(selection)
+    .replace(escapedCharacterRegExp, "\\\\")
+    .replace(interpolatedVarRegExp, "\\$");
   editor.insertSnippet(new SnippetString(snippet(widget)), selection);
   await commands.executeCommand("editor.action.formatDocument");
 };
