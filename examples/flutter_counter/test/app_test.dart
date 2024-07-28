db714f374fe315dd78d644ad56daa003aca58de7
Felix Angelov <felangelov@gmail.com>
2024-07-28 15:50:21 -0500

    refactor(flutter_counter): improve test descriptions

--- examples/flutter_counter/test/app_test.dart
+++ examples/flutter_counter/test/app_test.dart
@@ -5,16 +5,16 @@ import 'package:flutter_counter/counter/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('CounterApp', () {
-    testWidgets('is a MaterialApp', (tester) async {
+  group(CounterApp, () {
+    testWidgets('is a $MaterialApp', (tester) async {
       expect(CounterApp(), isA<MaterialApp>());
     });
 
-    testWidgets('home is CounterPage', (tester) async {
+    testWidgets('home is $CounterPage', (tester) async {
       expect(CounterApp().home, isA<CounterPage>());
     });
 
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders $CounterPage', (tester) async {
       await tester.pumpWidget(CounterApp());
       expect(find.byType(CounterPage), findsOneWidget);
     });
--- examples/flutter_counter/test/counter/view/counter_page_test.dart
+++ examples/flutter_counter/test/counter/view/counter_page_test.dart
@@ -4,8 +4,8 @@ import 'package:flutter_counter/counter/counter.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('CounterPage', () {
-    testWidgets('renders CounterView', (tester) async {
+  group(CounterPage, () {
+    testWidgets('renders $CounterView', (tester) async {
       await tester.pumpWidget(MaterialApp(home: CounterPage()));
       expect(find.byType(CounterView), findsOneWidget);
     });
