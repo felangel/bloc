4477f74bb9102c650bc1d9b8846e2c533a152858
Felix Angelov <felangelov@gmail.com>
2024-07-28 15:48:23 -0500

    refactor(flutter_complex_list): improve test descriptions

--- examples/flutter_complex_list/test/complex_list/cubit/complex_list_state_test.dart
+++ examples/flutter_complex_list/test/complex_list/cubit/complex_list_state_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_complex_list/complex_list/complex_list.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('ComplexListState', () {
+  group(ComplexListState, () {
     const mockItems = [Item(id: '1', value: '1')];
     test('support value comparisons', () {
       expect(ComplexListState.loading(), ComplexListState.loading());
