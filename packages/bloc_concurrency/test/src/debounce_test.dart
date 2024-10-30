671136f884bc71497b6e5ffd2b65a71e408f1e25
Morgan <mrgnhnt96@gmail.com>
2024-10-30 07:19:09 -0700

    feat: add tests for assertion and leading arg

--- packages/bloc_concurrency/test/src/debounce_test.dart
+++ packages/bloc_concurrency/test/src/debounce_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_redundant_argument_values
+
 import 'package:bloc_concurrency/src/debounce.dart';
 import 'package:test/test.dart';
 
@@ -7,12 +9,13 @@ void main() {
   group('debounceFirst', () {
     test('should debounce all events', () async {
       final states = <int>[];
-      final bloc =
-          CounterBloc(debounce(duration: const Duration(milliseconds: 20)))
-            ..stream.listen(states.add)
-            ..add(Increment())
-            ..add(Increment())
-            ..add(Increment());
+      final bloc = CounterBloc(
+        debounce(duration: const Duration(milliseconds: 20)),
+      )
+        ..stream.listen(states.add)
+        ..add(Increment())
+        ..add(Increment())
+        ..add(Increment());
 
       await tick();
 
@@ -55,6 +58,74 @@ void main() {
       );
 
       expect(states, equals([1]));
+    });
+
+    test('should not debounce first event, and then debounce following events',
+        () async {
+      final states = <int>[];
+      final bloc = CounterBloc(
+        debounce(
+          duration: const Duration(milliseconds: 20),
+          leading: true,
+        ),
+      )
+        ..stream.listen(states.add)
+        ..add(Increment())
+        ..add(Increment())
+        ..add(Increment());
+
+      await tick();
+
+      expect(
+        bloc.onCalls,
+        equals([Increment()]),
+      );
+
+      await wait();
+
+      expect(
+        bloc.onEmitCalls,
+        equals([Increment()]),
+      );
+
+      expect(
+        bloc.onCalls,
+        equals([Increment(), Increment()]),
+      );
+
+      await wait();
+
+      expect(
+        bloc.onEmitCalls,
+        equals([Increment(), Increment()]),
+      );
+
+      expect(states, equals([1, 2]));
+
+      await bloc.close();
+
+      expect(
+        bloc.onCalls,
+        equals([Increment(), Increment()]),
+      );
+
+      expect(
+        bloc.onEmitCalls,
+        equals([Increment(), Increment()]),
+      );
+
+      expect(states, equals([1, 2]));
+    });
+
+    test('should throw assertion error when duration is negative', () {
+      expect(
+        () => CounterBloc(
+          debounce(
+            duration: const Duration(milliseconds: -20),
+          ),
+        ),
+        throwsA(isA<AssertionError>()),
+      );
     });
   });
 }
