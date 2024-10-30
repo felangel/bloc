5a68a2ca43102341cbaf3c748f4da0aa0aa8235a
Morgan <mrgnhnt96@gmail.com>
2024-10-30 07:59:38 -0700

    feat: clean up tests, add tests for transformer arg

--- packages/bloc_concurrency/test/src/debounce_test.dart
+++ packages/bloc_concurrency/test/src/debounce_test.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: avoid_redundant_argument_values
 
+import 'package:bloc_concurrency/src/concurrent.dart';
 import 'package:bloc_concurrency/src/debounce.dart';
+import 'package:bloc_concurrency/src/sequential.dart';
 import 'package:test/test.dart';
 
 import 'helpers.dart';
@@ -19,45 +21,21 @@ void main() {
 
       await tick();
 
-      expect(
-        bloc.onCalls,
-        equals([]),
-      );
+      expect(bloc.onCalls, isEmpty);
+      expect(bloc.onEmitCalls, isEmpty);
 
       await wait();
 
-      expect(
-        bloc.onEmitCalls,
-        equals([]),
-      );
-
-      expect(
-        bloc.onCalls,
-        equals([Increment()]),
-      );
+      expect(bloc.onEmitCalls, isEmpty);
+      expect(bloc.onCalls, hasLength(1));
 
       await wait();
 
-      expect(
-        bloc.onEmitCalls,
-        equals([Increment()]),
-      );
-
-      expect(states, equals([1]));
+      expect(bloc.onCalls, hasLength(1));
+      expect(bloc.onEmitCalls, hasLength(1));
+      expect(states, [1]);
 
       await bloc.close();
-
-      expect(
-        bloc.onCalls,
-        equals([Increment()]),
-      );
-
-      expect(
-        bloc.onEmitCalls,
-        equals([Increment()]),
-      );
-
-      expect(states, equals([1]));
     });
 
     test('should not debounce first event, and then debounce following events',
@@ -76,45 +54,99 @@ void main() {
 
       await tick();
 
-      expect(
-        bloc.onCalls,
-        equals([Increment()]),
-      );
+      expect(bloc.onCalls, hasLength(1));
+      expect(bloc.onEmitCalls, isEmpty);
 
       await wait();
 
-      expect(
-        bloc.onEmitCalls,
-        equals([Increment()]),
-      );
-
-      expect(
-        bloc.onCalls,
-        equals([Increment(), Increment()]),
-      );
+      expect(bloc.onCalls, hasLength(2));
+      expect(bloc.onEmitCalls, hasLength(1));
 
       await wait();
 
-      expect(
-        bloc.onEmitCalls,
-        equals([Increment(), Increment()]),
-      );
-
+      expect(bloc.onCalls, hasLength(2));
+      expect(bloc.onEmitCalls, hasLength(2));
       expect(states, equals([1, 2]));
 
       await bloc.close();
+    });
 
-      expect(
-        bloc.onCalls,
-        equals([Increment(), Increment()]),
-      );
+    group('transformer', () {
+      test('concurrent should process events all at once', () async {
+        final states = <int>[];
+        final bloc = CounterBloc(
+          debounce(
+            duration: const Duration(milliseconds: 5),
+            transformer: concurrent(),
+          ),
+        )
+          ..stream.listen(states.add)
+          ..add(Increment());
 
-      expect(
-        bloc.onEmitCalls,
-        equals([Increment(), Increment()]),
-      );
+        expect(bloc.onCalls, isEmpty);
+        expect(bloc.onEmitCalls, isEmpty);
 
-      expect(states, equals([1, 2]));
+        // Add events after debounce period
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+        bloc.add(Increment());
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+        bloc.add(Increment());
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+
+        expect(bloc.onCalls, hasLength(3));
+        expect(bloc.onEmitCalls, isEmpty);
+        expect(states, isEmpty);
+
+        await wait();
+
+        expect(bloc.onEmitCalls, hasLength(3));
+        expect(states, [1, 2, 3]);
+      });
+
+      test('sequential should process events one at a time', () async {
+        final states = <int>[];
+        final bloc = CounterBloc(
+          debounce(
+            duration: const Duration(milliseconds: 5),
+            transformer: sequential(),
+          ),
+        )
+          ..stream.listen(states.add)
+          ..add(Increment())
+          ..add(Increment());
+
+        expect(bloc.onCalls, isEmpty);
+        expect(bloc.onEmitCalls, isEmpty);
+
+        // Add events after debounce period
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+        bloc
+          ..add(Increment())
+          ..add(Increment());
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+        bloc
+          ..add(Increment())
+          ..add(Increment());
+        await Future<void>.delayed(const Duration(milliseconds: 10));
+
+        var onCallsCount = 1;
+        var onEmitCallsCount = 0;
+
+        for (var i = 0; i < 3; i++) {
+          expect(bloc.onCalls, hasLength(onCallsCount));
+          expect(bloc.onEmitCalls, hasLength(onEmitCallsCount));
+          expect(states, [1, 2, 3].take(i));
+
+          onCallsCount++;
+          onEmitCallsCount++;
+
+          await wait();
+        }
+
+        expect(bloc.onCalls, hasLength(3));
+        expect(bloc.onEmitCalls, hasLength(3));
+        expect(states, [1, 2, 3]);
+      });
     });
 
     test('should throw assertion error when duration is negative', () {
