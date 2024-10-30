e0fdf0a3527d482fcd4e79c166a2ac6189ed3f81
Morgan <mrgnhnt96@gmail.com>
2024-10-30 07:18:42 -0700

    refactor: remove trailing paramter to always process last event

--- packages/bloc_concurrency/lib/src/debounce.dart
+++ packages/bloc_concurrency/lib/src/debounce.dart
@@ -15,25 +15,19 @@ import 'package:stream_transform/stream_transform.dart';
 /// The [leading] parameter determines whether the first event in a sequence
 /// should be emitted immediately. By default, [leading] is set to `false`.
 ///
-/// The [trailing] parameter determines whether the last event in a sequence
-/// should be emitted after the [duration] expires.
-/// By default, [trailing] is set to `true`.
-///
 /// **Note**: debounced events never trigger the event handler.
 EventTransformer<E> debounce<E>({
   required Duration duration,
   bool leading = false,
-  bool trailing = true,
 }) {
   assert(duration >= Duration.zero, 'duration cannot be negative');
-  assert(leading || trailing, 'leading or trailing must be true');
 
   return (events, mapper) {
     return concurrent<E>().call(
       events.debounce(
         duration,
         leading: leading,
-        trailing: trailing,
+        trailing: true,
       ),
       mapper,
     );
