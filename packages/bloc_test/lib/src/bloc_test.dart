ed7b27c3d1e57b82e612a98f820f58a8f2770ed6
Felix Angelov <felangelov@gmail.com>
2025-01-01 15:58:46 -0600

    refactor(bloc)!: introduce `EmittableStateStreamableSource` (#4311)

--- packages/bloc_test/lib/src/bloc_test.dart
+++ packages/bloc_test/lib/src/bloc_test.dart
@@ -136,7 +136,7 @@ import 'package:test/test.dart' as test;
 ///
 /// [configuring tags]: https://github.com/dart-lang/test/blob/master/pkgs/test/doc/configuration.md#configuring-tags
 @isTest
-void blocTest<B extends BlocBase<State>, State>(
+void blocTest<B extends EmittableStateStreamableSource<State>, State>(
   String description, {
   required B Function() build,
   FutureOr<void> Function()? setUp,
@@ -173,7 +173,7 @@ void blocTest<B extends BlocBase<State>, State>(
 /// Internal [blocTest] runner which is only visible for testing.
 /// This should never be used directly -- please use [blocTest] instead.
 @visibleForTesting
-Future<void> testBloc<B extends BlocBase<State>, State>({
+Future<void> testBloc<B extends EmittableStateStreamableSource<State>, State>({
   required B Function() build,
   FutureOr<void> Function()? setUp,
   State Function()? seed,
