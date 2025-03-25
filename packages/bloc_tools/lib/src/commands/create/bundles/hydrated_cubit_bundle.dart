c9f7ebb5320eab3983d0c8d70f009580c0387b19
Felix Angelov <felangelov@gmail.com>
2025-03-24 23:54:55 -0500

    feat(bloc_tools): add `bloc create` command

---
+++ packages/bloc_tools/lib/src/commands/create/bundles/hydrated_cubit_bundle.dart
@@ -0,0 +1,111 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, implicit_dynamic_list_literal, implicit_dynamic_map_literal, inference_failure_on_collection_literal
+
+import 'package:mason/mason.dart';
+
+final hydratedCubitBundle = MasonBundle.fromJson(<String, dynamic>{
+  "files": [
+    {
+      "path": "{{name.snakeCase()}}_cubit.dart",
+      "data":
+          "e3sjdXNlX2ZyZWV6ZWR9fXt7PiBmcmVlemVkX2N1Yml0IH19e3svdXNlX2ZyZWV6ZWR9fXt7I3VzZV9lcXVhdGFibGV9fXt7PiBlcXVhdGFibGVfY3ViaXQgfX17ey91c2VfZXF1YXRhYmxlfX17eyN1c2VfYmFzaWN9fXt7PiBiYXNpY19jdWJpdCB9fXt7L3VzZV9iYXNpY319",
+      "type": "text",
+    },
+    {
+      "path": "{{name.snakeCase()}}_state.dart",
+      "data":
+          "e3sjdXNlX2ZyZWV6ZWR9fXt7PiBmcmVlemVkX3N0YXRlIH19e3svdXNlX2ZyZWV6ZWR9fXt7I3VzZV9lcXVhdGFibGV9fXt7PiBlcXVhdGFibGVfc3RhdGUgfX17ey91c2VfZXF1YXRhYmxlfX17eyN1c2VfYmFzaWN9fXt7PiBiYXNpY19zdGF0ZSB9fXt7L3VzZV9iYXNpY319",
+      "type": "text",
+    },
+    {
+      "path": "{{~ basic_cubit }}",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOmh5ZHJhdGVkX2Jsb2MvaHlkcmF0ZWRfYmxvYy5kYXJ0JzsKCnBhcnQgJ3t7bmFtZS5zbmFrZUNhc2UoKX19X3N0YXRlLmRhcnQnOwoKY2xhc3Mge3tuYW1lLnBhc2NhbENhc2UoKX19Q3ViaXQgZXh0ZW5kcyBIeWRyYXRlZEN1Yml0PHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlPiB7CiAge3tuYW1lLnBhc2NhbENhc2UoKX19Q3ViaXQoKSA6IHN1cGVyKGNvbnN0IHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlKCkpOwoKICBAb3ZlcnJpZGUKICBNYXA8U3RyaW5nLCBkeW5hbWljPiB0b0pzb24oe3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUgc3RhdGUpIHsKICAgIC8vIFRPRE86IGltcGxlbWVudCB0b0pzb24KICB9CgogIEBvdmVycmlkZQogIHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlIGZyb21Kc29uKE1hcDxTdHJpbmcsIGR5bmFtaWM+IGpzb24pIHsKICAgIC8vIFRPRE86IGltcGxlbWVudCBmcm9tSnNvbgogIH0KfQo=",
+      "type": "text",
+    },
+    {
+      "path": "{{~ basic_state }}",
+      "data":
+          "cGFydCBvZiAne3tuYW1lLnNuYWtlQ2FzZSgpfX1fY3ViaXQuZGFydCc7CgpjbGFzcyB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZSB7CiAgY29uc3Qge3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUoKTsKfQo=",
+      "type": "text",
+    },
+    {
+      "path": "{{~ equatable_cubit }}",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOmVxdWF0YWJsZS9lcXVhdGFibGUuZGFydCc7CmltcG9ydCAncGFja2FnZTpoeWRyYXRlZF9ibG9jL2h5ZHJhdGVkX2Jsb2MuZGFydCc7CgpwYXJ0ICd7e25hbWUuc25ha2VDYXNlKCl9fV9zdGF0ZS5kYXJ0JzsKCmNsYXNzIHt7bmFtZS5wYXNjYWxDYXNlKCl9fUN1Yml0IGV4dGVuZHMgSHlkcmF0ZWRDdWJpdDx7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZT4gewogIHt7bmFtZS5wYXNjYWxDYXNlKCl9fUN1Yml0KCkgOiBzdXBlcihjb25zdCB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZSgpKTsKCiAgQG92ZXJyaWRlCiAgTWFwPFN0cmluZywgZHluYW1pYz4gdG9Kc29uKHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlIHN0YXRlKSB7CiAgICAvLyBUT0RPOiBpbXBsZW1lbnQgdG9Kc29uCiAgfQoKICBAb3ZlcnJpZGUKICB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZSBmcm9tSnNvbihNYXA8U3RyaW5nLCBkeW5hbWljPiBqc29uKSB7CiAgICAvLyBUT0RPOiBpbXBsZW1lbnQgZnJvbUpzb24KICB9Cn0K",
+      "type": "text",
+    },
+    {
+      "path": "{{~ equatable_state }}",
+      "data":
+          "cGFydCBvZiAne3tuYW1lLnNuYWtlQ2FzZSgpfX1fY3ViaXQuZGFydCc7CgpjbGFzcyB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZSBleHRlbmRzIEVxdWF0YWJsZSB7CiAgY29uc3Qge3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUoKTsKCiAgQG92ZXJyaWRlCiAgTGlzdDxPYmplY3Q+IGdldCBwcm9wcyA9PiBbXTsKfQo=",
+      "type": "text",
+    },
+    {
+      "path": "{{~ freezed_cubit }}",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOmZyZWV6ZWRfYW5ub3RhdGlvbi9mcmVlemVkX2Fubm90YXRpb24uZGFydCc7CmltcG9ydCAncGFja2FnZTpoeWRyYXRlZF9ibG9jL2h5ZHJhdGVkX2Jsb2MuZGFydCc7CgpwYXJ0ICd7e25hbWUuc25ha2VDYXNlKCl9fV9zdGF0ZS5kYXJ0JzsKcGFydCAne3tuYW1lLnNuYWtlQ2FzZSgpfX1fY3ViaXQuZnJlZXplZC5kYXJ0JzsKCmNsYXNzIHt7bmFtZS5wYXNjYWxDYXNlKCl9fUN1Yml0IGV4dGVuZHMgSHlkcmF0ZWRDdWJpdDx7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZT4gewogIHt7bmFtZS5wYXNjYWxDYXNlKCl9fUN1Yml0KCkgOiBzdXBlcihjb25zdCB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZS5pbml0aWFsKCkpOwoKICBAb3ZlcnJpZGUKICBNYXA8U3RyaW5nLCBkeW5hbWljPiB0b0pzb24oe3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUgc3RhdGUpIHsKICAgIC8vIFRPRE86IGltcGxlbWVudCB0b0pzb24KICB9CgogIEBvdmVycmlkZQogIHt7bmFtZS5wYXNjYWxDYXNlKCl9fVN0YXRlIGZyb21Kc29uKE1hcDxTdHJpbmcsIGR5bmFtaWM+IGpzb24pIHsKICAgIC8vIFRPRE86IGltcGxlbWVudCBmcm9tSnNvbgogIH0KfQo=",
+      "type": "text",
+    },
+    {
+      "path": "{{~ freezed_state }}",
+      "data":
+          "cGFydCBvZiAne3tuYW1lLnNuYWtlQ2FzZSgpfX1fY3ViaXQuZGFydCc7CgpAZnJlZXplZApjbGFzcyB7e25hbWUucGFzY2FsQ2FzZSgpfX1TdGF0ZSB3aXRoIF8ke3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUgewogIGNvbnN0IGZhY3Rvcnkge3tuYW1lLnBhc2NhbENhc2UoKX19U3RhdGUuaW5pdGlhbCgpID0gX0luaXRpYWw7Cn0K",
+      "type": "text",
+    },
+  ],
+  "hooks": [
+    {
+      "path": "pre_gen.dart",
+      "data":
+          "aW1wb3J0ICdwYWNrYWdlOm1hc29uL21hc29uLmRhcnQnOwoKRnV0dXJlPHZvaWQ+IHJ1bihIb29rQ29udGV4dCBjb250ZXh0KSBhc3luYyB7CiAgZmluYWwgc3R5bGUgPSBjb250ZXh0LnZhcnNbJ3N0eWxlJ107CiAgY29udGV4dC52YXJzID0gewogICAgLi4uY29udGV4dC52YXJzLAogICAgJ3VzZV9iYXNpYyc6IHN0eWxlID09ICdiYXNpYycsCiAgICAndXNlX2VxdWF0YWJsZSc6IHN0eWxlID09ICdlcXVhdGFibGUnLAogICAgJ3VzZV9mcmVlemVkJzogc3R5bGUgPT0gJ2ZyZWV6ZWQnLAogIH07Cn0K",
+      "type": "text",
+    },
+    {
+      "path": "pubspec.yaml",
+      "data":
+          "bmFtZTogaHlkcmF0ZWRfY3ViaXRfaG9va3MKCmVudmlyb25tZW50OgogIHNkazogXjMuNS40CgpkZXBlbmRlbmNpZXM6CiAgbWFzb246IF4wLjEuMAo=",
+      "type": "text",
+    },
+  ],
+  "name": "hydrated_cubit",
+  "description":
+      "Generate a new HydratedCubit in Dart. Built for the bloc state management library.",
+  "version": "0.3.0",
+  "environment": {"mason": "^0.1.0"},
+  "repository":
+      "https://github.com/felangel/bloc/tree/master/bricks/hydrated_cubit",
+  "readme": {
+    "path": "README.md",
+    "data":
+        "PHAgYWxpZ249ImNlbnRlciI+CjxpbWcgc3R5bGU9ImhlaWdodDoxMDBweCIgc3JjPSJodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vZmVsYW5nZWwvYmxvYy9tYXN0ZXIvYXNzZXRzL2xvZ29zL2h5ZHJhdGVkX2N1Yml0X2RhcmsucG5nIiBhbHQ9Ikh5ZHJhdGVkIEN1Yml0IiAvPgo8L3A+Cgo8cCBhbGlnbj0iY2VudGVyIj4KPGEgaHJlZj0iaHR0cHM6Ly9naXRodWIuY29tL2ZlbGFuZ2VsL2Jsb2MvYWN0aW9ucyI+PGltZyBzcmM9Imh0dHBzOi8vZ2l0aHViLmNvbS9mZWxhbmdlbC9ibG9jL3dvcmtmbG93cy9idWlsZC9iYWRnZS5zdmciIGFsdD0iYnVpbGQiPjwvYT4KPGEgaHJlZj0iaHR0cHM6Ly9jb2RlY292LmlvL2doL2ZlbGFuZ2VsL2Jsb2MiPjxpbWcgc3JjPSJodHRwczovL2NvZGVjb3YuaW8vZ2gvZmVsYW5nZWwvQmxvYy9icmFuY2gvbWFzdGVyL2dyYXBoL2JhZGdlLnN2ZyIgYWx0PSJjb2RlY292Ij48L2E+CjxhIGhyZWY9Imh0dHBzOi8vZ2l0aHViLmNvbS9mZWxhbmdlbC9ibG9jIj48aW1nIHNyYz0iaHR0cHM6Ly9pbWcuc2hpZWxkcy5pby9naXRodWIvc3RhcnMvZmVsYW5nZWwvYmxvYy5zdmc/c3R5bGU9ZmxhdCZsb2dvPWdpdGh1YiZjb2xvckI9ZGVlcHBpbmsmbGFiZWw9c3RhcnMiIGFsdD0iU3RhciBvbiBHaXRodWIiPjwvYT4KPGEgaHJlZj0iaHR0cHM6Ly9vcGVuc291cmNlLm9yZy9saWNlbnNlcy9NSVQiPjxpbWcgc3JjPSJodHRwczovL2ltZy5zaGllbGRzLmlvL2JhZGdlL2xpY2Vuc2UtTUlULXB1cnBsZS5zdmciIGFsdD0iTGljZW5zZTogTUlUIj48L2E+CjxhIGhyZWY9Imh0dHBzOi8vZGlzY29yZC5nZy9ibG9jIj48aW1nIHNyYz0iaHR0cHM6Ly9pbWcuc2hpZWxkcy5pby9kaXNjb3JkLzY0OTcwODc3ODYzMTIwMDc3OC5zdmc/bG9nbz1kaXNjb3JkJmNvbG9yPWJsdWUiIGFsdD0iRGlzY29yZCI+PC9hPgo8YSBocmVmPSJodHRwczovL2dpdGh1Yi5jb20vZmVsYW5nZWwvYmxvYyI+PGltZyBzcmM9Imh0dHBzOi8vdGlueXVybC5jb20vYmxvYy1saWJyYXJ5IiBhbHQ9IkJsb2MgTGlicmFyeSI+PC9hPgo8YSBocmVmPSJodHRwczovL2dpdGh1Yi5jb20vZmVsYW5nZWwvbWFzb24iPjxpbWcgc3JjPSJodHRwczovL2ltZy5zaGllbGRzLmlvL2VuZHBvaW50P3VybD1odHRwcyUzQSUyRiUyRnRpbnl1cmwuY29tJTJGbWFzb24tYmFkZ2UiIGFsdD0iUG93ZXJlZCBieSBNYXNvbiI+PC9hPgo8L3A+CgpHZW5lcmF0ZSBhIG5ldyBIeWRyYXRlZEN1Yml0IGluIFtEYXJ0XVsxXS4gQnVpbHQgZm9yIHRoZSBbYmxvYyBzdGF0ZSBtYW5hZ2VtZW50IGxpYnJhcnldWzJdLgoKIyMgVXNhZ2Ug8J+agAoKYGBgc2gKbWFzb24gbWFrZSBoeWRyYXRlZF9jdWJpdCAtLW5hbWUgY291bnRlciAtLXN0eWxlIGJhc2ljCmBgYAoKIyMgVmFyaWFibGVzIOKcqAoKfCBWYXJpYWJsZSB8IERlc2NyaXB0aW9uICAgICAgICAgICAgICAgICAgfCBEZWZhdWx0ICAgICAgICAgICAgICAgICAgICAgICAgICAgICB8IFR5cGUgICAgIHwKfCAtLS0tLS0tLSB8IC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0gfCAtLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSB8IC0tLS0tLS0tIHwKfCBgbmFtZWAgICB8IFRoZSBuYW1lIG9mIHRoZSBjdWJpdCBjbGFzcyAgfCBgY291bnRlcmAgICAgICAgICAgICAgICAgICAgICAgICAgICB8IGBzdHJpbmdgIHwKfCBgc3R5bGVgICB8IFRoZSBzdHlsZSBvZiBjdWJpdCBnZW5lcmF0ZWQgfCBgYmFzaWMgKGJhc2ljLCBlcXVhdGFibGUsIGZyZWV6ZWQpYCB8IGBlbnVtYCAgIHwKCiMjIE91dHB1dCDwn5OmCgpgYGBzaArilJzilIDilIAgY291bnRlcl9jdWJpdC5kYXJ0CuKUlOKUgOKUgCBjb3VudGVyX3N0YXRlLmRhcnQKYGBgCgpbMV06IGh0dHBzOi8vZGFydC5kZXYKWzJdOiBodHRwczovL2dpdGh1Yi5jb20vZmVsYW5nZWwvYmxvYwo=",
+    "type": "text",
+  },
+  "changelog": {
+    "path": "CHANGELOG.md",
+    "data":
+        "IyAwLjMuMAoKLSBjaG9yZShkZXBzKTogdXBncmFkZSB0byBgbWFzb24gXjAuMS4wYAotIGNob3JlKGRlcHMpOiB1cGdyYWRlIGhvb2tzIHRvIGBkYXJ0IF4zLjUuNGAKCiMgMC4yLjEKCi0gY2hvcmU6IHVwZGF0ZSBjb3B5cmlnaHQgeWVhcgotIGNob3JlOiB1cGRhdGUgbG9nbyBpbWFnZSByZWZzCgojIDAuMi4wCgotIGZlYXQ6IGFkZCBzdXBwb3J0IGZvciBgZXF1YXRhYmxlYAotIGZlYXQ6IGFkZCBzdXBwb3J0IGZvciBgZnJlZXplZGAKCiMgMC4xLjMKCi0gZml4OiBwYXJ0IGFuZCBpbXBvcnRzCgojIDAuMS4yCgotIGRvY3M6IGFkZCBiYWRnZXMgdG8gUkVBRE1FCi0gZG9jczogdXNlIGRhcmsgbG9nbyB2YXJpYW50CgojIDAuMS4xCgotIGRvY3M6IG1pbm9yIFJFQURNRSB1cGRhdGUKCiMgMC4xLjAKCi0gZmVhdDogaW5pdGlhbCByZWxlYXNlIHdpdGggc3VwcG9ydCBmb3IgYmFzaWMgaHlkcmF0ZWQgY3ViaXQgZ2VuZXJhdGlvbgo=",
+    "type": "text",
+  },
+  "license": {
+    "path": "LICENSE",
+    "data":
+        "TUlUIExpY2Vuc2UKCkNvcHlyaWdodCAoYykgMjAyNCBGZWxpeCBBbmdlbG92CgpQZXJtaXNzaW9uIGlzIGhlcmVieSBncmFudGVkLCBmcmVlIG9mIGNoYXJnZSwgdG8gYW55IHBlcnNvbiBvYnRhaW5pbmcgYSBjb3B5Cm9mIHRoaXMgc29mdHdhcmUgYW5kIGFzc29jaWF0ZWQgZG9jdW1lbnRhdGlvbiBmaWxlcyAodGhlICJTb2Z0d2FyZSIpLCB0byBkZWFsCmluIHRoZSBTb2Z0d2FyZSB3aXRob3V0IHJlc3RyaWN0aW9uLCBpbmNsdWRpbmcgd2l0aG91dCBsaW1pdGF0aW9uIHRoZSByaWdodHMKdG8gdXNlLCBjb3B5LCBtb2RpZnksIG1lcmdlLCBwdWJsaXNoLCBkaXN0cmlidXRlLCBzdWJsaWNlbnNlLCBhbmQvb3Igc2VsbApjb3BpZXMgb2YgdGhlIFNvZnR3YXJlLCBhbmQgdG8gcGVybWl0IHBlcnNvbnMgdG8gd2hvbSB0aGUgU29mdHdhcmUgaXMKZnVybmlzaGVkIHRvIGRvIHNvLCBzdWJqZWN0IHRvIHRoZSBmb2xsb3dpbmcgY29uZGl0aW9uczoKClRoZSBhYm92ZSBjb3B5cmlnaHQgbm90aWNlIGFuZCB0aGlzIHBlcm1pc3Npb24gbm90aWNlIHNoYWxsIGJlIGluY2x1ZGVkIGluIGFsbApjb3BpZXMgb3Igc3Vic3RhbnRpYWwgcG9ydGlvbnMgb2YgdGhlIFNvZnR3YXJlLgoKVEhFIFNPRlRXQVJFIElTIFBST1ZJREVEICJBUyBJUyIsIFdJVEhPVVQgV0FSUkFOVFkgT0YgQU5ZIEtJTkQsIEVYUFJFU1MgT1IKSU1QTElFRCwgSU5DTFVESU5HIEJVVCBOT1QgTElNSVRFRCBUTyBUSEUgV0FSUkFOVElFUyBPRiBNRVJDSEFOVEFCSUxJVFksCkZJVE5FU1MgRk9SIEEgUEFSVElDVUxBUiBQVVJQT1NFIEFORCBOT05JTkZSSU5HRU1FTlQuIElOIE5PIEVWRU5UIFNIQUxMIFRIRQpBVVRIT1JTIE9SIENPUFlSSUdIVCBIT0xERVJTIEJFIExJQUJMRSBGT1IgQU5ZIENMQUlNLCBEQU1BR0VTIE9SIE9USEVSCkxJQUJJTElUWSwgV0hFVEhFUiBJTiBBTiBBQ1RJT04gT0YgQ09OVFJBQ1QsIFRPUlQgT1IgT1RIRVJXSVNFLCBBUklTSU5HIEZST00sCk9VVCBPRiBPUiBJTiBDT05ORUNUSU9OIFdJVEggVEhFIFNPRlRXQVJFIE9SIFRIRSBVU0UgT1IgT1RIRVIgREVBTElOR1MgSU4gVEhFClNPRlRXQVJFLgo=",
+    "type": "text",
+  },
+  "vars": {
+    "name": {
+      "type": "string",
+      "description": "The name of the cubit class.",
+      "default": "counter",
+      "prompt": "Please enter the cubit name.",
+    },
+    "style": {
+      "type": "enum",
+      "description": "The style of cubit generated.",
+      "default": "basic",
+      "prompt": "What is the cubit style?",
+      "values": ["basic", "equatable", "freezed"],
+    },
+  },
+});
