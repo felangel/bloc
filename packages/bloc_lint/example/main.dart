fddfcf31f2099bfa7fd7adb9a1bbc044f3166d5d
Felix Angelov <felangelov@gmail.com>
2025-04-24 20:51:42 -0500

    comments

--- packages/bloc_lint/example/main.dart
+++ packages/bloc_lint/example/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: unused_local_variable
 import 'package:bloc_lint/bloc_lint.dart';
 
+// Usage: dart run main.dart ./path/to/analyze
 void main(List<String> args) {
+  // Analyze the provided file or directory and report all diagnostics.
   final diagnostics = const Linter().analyze(uri: Uri.parse(args.first));
 }
