86ee2c44a40459f9ec1f474932f6d0c9b034f68c
Felix Angelov <felangelov@gmail.com>
2025-05-28 19:38:23 -0700

    fix windows

--- packages/bloc_lint/lib/src/analysis_options.dart
+++ packages/bloc_lint/lib/src/analysis_options.dart
@@ -104,7 +104,7 @@ class AnalysisOptions {
     if (fullUri == null) return null;
     var path = include.split(packagePrefix).last;
     if (path.startsWith(p.separator)) path = path.substring(1);
-    var resolvedPath = fullUri.path + path;
+    var resolvedPath = p.fromUri(fullUri) + path;
     if (!p.isAbsolute(resolvedPath)) {
       resolvedPath = p.join(packageConfigFile.parent.path, resolvedPath);
     }
