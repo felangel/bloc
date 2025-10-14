6c4ddacfdd64fc358bb85769c43b7b2485b3ac8f
sh1l0n <jesusmanresaparres@protonmail.com>
2025-10-14 18:01:18 +0200

    Added BlocListenerWithTransition

---
+++ packages/flutter_bloc/lib/src/bloc_listener_with_transition.dart
@@ -0,0 +1,227 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/single_child_widget.dart';
+
+/// Signature for the `listener` function which takes the `BuildContext` along
+/// with the previous and current `state` and is responsible for executing in response to
+/// `state` changes.
+typedef BlocWidgetListenerWithTransition<S> =
+    void Function(BuildContext context, S previous, S current);
+
+/// Signature for the `listenWhen` function which takes the previous `state`
+/// and the current `state` and is responsible for returning a [bool] which
+/// determines whether or not to call [BlocWidgetListenerWithTransition] of [BlocListener]
+/// with the current `state`.
+typedef BlocListenerWithTransitionCondition<S> =
+    bool Function(S previous, S current);
+
+/// {@template bloc_listener}
+/// Takes a [BlocWidgetListenerWithTransition] and an optional [bloc] and invokes
+/// the [listener] in response to `state` changes in the [bloc].
+/// It should be used for functionality that needs to occur only in response to
+/// a `state` change such as navigation, showing a `SnackBar`, showing
+/// a `Dialog`, etc...
+/// The [listener] is guaranteed to only be called once for each `state` change
+/// unlike the `builder` in `BlocBuilder`.
+///
+/// If the [bloc] parameter is omitted, [BlocListener] will automatically
+/// perform a lookup using [BlocProvider] and the current `BuildContext`.
+///
+/// ```dart
+/// BlocListenerWithTransition<BlocA, BlocAState>(
+///   listener: (context, previous, current) {
+///     // do stuff here based on BlocA's state
+///   },
+///   child: Container(),
+/// )
+/// ```
+/// Only specify the [bloc] if you wish to provide a [bloc] that is otherwise
+/// not accessible via [BlocProvider] and the current `BuildContext`.
+///
+/// ```dart
+/// WithTransitionr<BlocA, BlocAState>(
+///   value: blocA,
+///   listener: (context, previous, current) {
+///     // do stuff here based on BlocA's state
+///   },
+///   child: Container(),
+/// )
+/// ```
+/// {@endtemplate}
+///
+/// {@template bloc_listener_listen_when}
+/// An optional [listenWhen] can be implemented for more granular control
+/// over when [listener] is called.
+/// [listenWhen] will be invoked on each [bloc] `state` change.
+/// [listenWhen] takes the previous `state` and current `state` and must
+/// return a [bool] which determines whether or not the [listener] function
+/// will be invoked.
+/// The previous `state` will be initialized to the `state` of the [bloc]
+/// when the [BlocListener] is initialized.
+/// [listenWhen] is optional and if omitted, it will default to `true`.
+///
+/// ```dart
+/// BlocListenerWithTransition<BlocA, BlocAState>(
+///   listenWhen: (previous, current) {
+///     // return true/false to determine whether or not
+///     // to invoke listener with state
+///   },
+///   listener: (context, previous, current) {
+///     // do stuff here based on BlocA's state
+///   },
+///   child: Container(),
+/// )
+/// ```
+/// {@endtemplate}
+class BlocListenerWithTransition<B extends StateStreamable<S>, S>
+    extends BlocListenerWithTransitionBase<B, S> {
+  /// {@macro bloc_listener}
+  /// {@macro bloc_listener_listen_when}
+  const BlocListenerWithTransition({
+    required super.listener,
+    super.key,
+    super.bloc,
+    super.listenWhen,
+    super.child,
+  });
+}
+
+/// {@template bloc_listener_base}
+/// Base class for widgets that listen to state changes in a specified [bloc].
+///
+/// A [BlocListenerBase] is stateful and maintains the state subscription.
+/// The type of the state and what happens with each state change
+/// is defined by sub-classes.
+/// {@endtemplate}
+abstract class BlocListenerWithTransitionBase<B extends StateStreamable<S>, S>
+    extends SingleChildStatefulWidget {
+  /// {@macro bloc_listener_base}
+  const BlocListenerWithTransitionBase({
+    required this.listener,
+    super.key,
+    this.bloc,
+    this.child,
+    this.listenWhen,
+  }) : super(child: child);
+
+  /// The widget which will be rendered as a descendant of the
+  /// [BlocListenerBase].
+  final Widget? child;
+
+  /// The [bloc] whose `state` will be listened to.
+  /// Whenever the [bloc]'s `state` changes, [listener] will be invoked.
+  final B? bloc;
+
+  /// The [BlocWidgetListenerWithTransition] which will be called on every `state` change.
+  /// This [listener] should be used for any code which needs to execute
+  /// in response to a `state` change.
+  final BlocWidgetListenerWithTransition<S> listener;
+
+  /// {@macro bloc_listener_listen_when}
+  final BlocListenerWithTransitionCondition<S>? listenWhen;
+
+  @override
+  SingleChildState<BlocListenerWithTransitionBase<B, S>> createState() =>
+      _BlocListenerWithTransitionBaseState<B, S>();
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty<B?>('bloc', bloc))
+      ..add(
+        ObjectFlagProperty<BlocWidgetListenerWithTransition<S>>.has(
+          'listener',
+          listener,
+        ),
+      )
+      ..add(
+        ObjectFlagProperty<BlocListenerWithTransitionCondition<S>?>.has(
+          'listenWhen',
+          listenWhen,
+        ),
+      );
+  }
+}
+
+class _BlocListenerWithTransitionBaseState<B extends StateStreamable<S>, S>
+    extends SingleChildState<BlocListenerWithTransitionBase<B, S>> {
+  StreamSubscription<S>? _subscription;
+  late B _bloc;
+  late S _previousState;
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = widget.bloc ?? context.read<B>();
+    _previousState = _bloc.state;
+    _subscribe();
+  }
+
+  @override
+  void didUpdateWidget(BlocListenerWithTransitionBase<B, S> oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    final oldBloc = oldWidget.bloc ?? context.read<B>();
+    final currentBloc = widget.bloc ?? oldBloc;
+    if (oldBloc != currentBloc) {
+      if (_subscription != null) {
+        _unsubscribe();
+        _bloc = currentBloc;
+        _previousState = _bloc.state;
+      }
+      _subscribe();
+    }
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final bloc = widget.bloc ?? context.read<B>();
+    if (_bloc != bloc) {
+      if (_subscription != null) {
+        _unsubscribe();
+        _bloc = bloc;
+        _previousState = _bloc.state;
+      }
+      _subscribe();
+    }
+  }
+
+  @override
+  Widget buildWithChild(BuildContext context, Widget? child) {
+    assert(
+      child != null,
+      '''${widget.runtimeType} used outside of MultiBlocListener must specify a child''',
+    );
+    if (widget.bloc == null) {
+      // Trigger a rebuild if the bloc reference has changed.
+      // See https://github.com/felangel/bloc/issues/2127.
+      context.select<B, bool>((bloc) => identical(_bloc, bloc));
+    }
+    return child!;
+  }
+
+  @override
+  void dispose() {
+    _unsubscribe();
+    super.dispose();
+  }
+
+  void _subscribe() {
+    _subscription = _bloc.stream.listen((state) {
+      if (!mounted) return;
+      if (widget.listenWhen?.call(_previousState, state) ?? true) {
+        widget.listener(context, _previousState, state);
+      }
+      _previousState = state;
+    });
+  }
+
+  void _unsubscribe() {
+    _subscription?.cancel();
+    _subscription = null;
+  }
+}
