37614800219535e845132bd83bca769e562b1f14
Felix Angelov <felangelov@gmail.com>
2026-02-07 16:03:58 -0600

    refactor: reorganize tests

--- packages/bloc_test/test/bloc_bloc_test_test.dart
+++ packages/bloc_test/test/bloc_bloc_test_test.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -8,8 +7,6 @@ import 'package:test/test.dart';
 import 'blocs/blocs.dart';
 
 class MockRepository extends Mock implements Repository {}
-
-class _MockBlocObserver extends Mock implements BlocObserver {}
 
 void unawaited(Future<void>? _) {}
 
@@ -664,107 +661,6 @@ Alternatively, consider using Matchers in the expect of the blocTest rather than
         expect((actualError as TestFailure).message, expectedError);
       });
     });
-  });
-
-  group('BlocObserver', () {
-    late BlocObserver blocObserver;
-
-    setUp(() {
-      blocObserver = _MockBlocObserver();
-      final previousObserver = Bloc.observer;
-      addTearDown(() => Bloc.observer = previousObserver);
-      Bloc.observer = blocObserver;
-    });
-
-    blocTest<CounterBloc, int>(
-      'calls onCreate',
-      build: () => CounterBloc(),
-      act: (bloc) => bloc.add(CounterEvent.increment),
-      verify: (bloc) {
-        // ignore: invalid_use_of_protected_member
-        verify(() => blocObserver.onCreate(bloc)).called(1);
-      },
-    );
-
-    blocTest<CounterBloc, int>(
-      'calls onEvent',
-      build: () => CounterBloc(),
-      act: (bloc) => bloc.add(CounterEvent.increment),
-      verify: (bloc) {
-        verify(
-          // ignore: invalid_use_of_protected_member
-          () => blocObserver.onEvent(bloc, CounterEvent.increment),
-        ).called(1);
-      },
-    );
-
-    blocTest<CounterBloc, int>(
-      'calls onChange',
-      build: () => CounterBloc(),
-      act: (bloc) => bloc.add(CounterEvent.increment),
-      verify: (bloc) {
-        const change = Change<int>(currentState: 0, nextState: 1);
-        // ignore: invalid_use_of_protected_member
-        verify(() => blocObserver.onChange(bloc, change)).called(1);
-      },
-    );
-
-    blocTest<CounterBloc, int>(
-      'calls onTransition',
-      build: () => CounterBloc(),
-      act: (bloc) => bloc.add(CounterEvent.increment),
-      verify: (bloc) {
-        const transition = Transition<CounterEvent, int>(
-          event: CounterEvent.increment,
-          currentState: 0,
-          nextState: 1,
-        );
-        // ignore: invalid_use_of_protected_member
-        verify(() => blocObserver.onTransition(bloc, transition)).called(1);
-      },
-    );
-
-    blocTest<ExceptionCounterBloc, int>(
-      'calls onError',
-      build: () => ExceptionCounterBloc(),
-      act: (bloc) => bloc.add(CounterEvent.increment),
-      setUp: () {
-        registerFallbackValue(StackTrace.empty);
-      },
-      verify: (bloc) {
-        verify(
-          // ignore: invalid_use_of_protected_member
-          () => blocObserver.onError(
-            bloc,
-            ExceptionCounterBlocException(),
-            any(),
-          ),
-        ).called(1);
-      },
-      errors: () => containsOnce(isA<ExceptionCounterBlocException>()),
-    );
-
-    blocTest<CounterBloc, int>(
-      'calls onDone',
-      build: () => CounterBloc(),
-      act: (bloc) => bloc.add(CounterEvent.increment),
-      verify: (bloc) {
-        verify(
-          // ignore: invalid_use_of_protected_member
-          () => blocObserver.onDone(bloc, CounterEvent.increment),
-        ).called(1);
-      },
-    );
-
-    blocTest<CounterBloc, int>(
-      'calls onClose',
-      build: () => CounterBloc(),
-      act: (bloc) => bloc.add(CounterEvent.increment),
-      verify: (bloc) {
-        // ignore: invalid_use_of_protected_member
-        verify(() => blocObserver.onClose(bloc)).called(1);
-      },
-    );
   });
 
   group('tearDown', () {
