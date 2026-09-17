c69fcd378c31c3d77811189a327e2b1796b6a910
Ronit Rameja <ronitrameja28@gmail.com>
2026-09-17 16:39:57 +0530

    feat(bloc_lint): add `avoid_mutable_states`

--- packages/bloc_lint/lib/bloc_lint.dart
+++ packages/bloc_lint/lib/bloc_lint.dart
@@ -10,6 +10,7 @@ export 'src/rules/rules.dart'
     show
         AvoidBuildContextExtensions,
         AvoidFlutterImports,
+        AvoidMutableStates,
         AvoidPublicBlocMethods,
         AvoidPublicFields,
         PreferBloc,
